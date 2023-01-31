7008bc25c23301011c39efcad23c5bbf3459ec66
rayapetkova <rayapetkova2007@abv.bg>
2023-01-31 16:21:18 +0200

    Update

---
+++ Python Fundamentals/Day 4 - lab (Functions)/01_absolute_values.py
@@ -0,0 +1,24 @@
+#1
+
+def abs_values():
+    lst = [abs(float(i)) for i in input().split()]
+    return lst
+
+
+print(abs_values())
+
+
+
+
+#2
+
+# def abs_values(new_list):
+#     lst = input().split()
+#     for i in lst:
+#         new_list.append(abs(float(i)))
+#     return new_list
+#
+#
+# new_list1 = []
+#
+# print(abs_values(new_list1))
