be55ecc68b2fd4f5a3cbc99be4b65f769a310b9c
rayapetkova <rayapetkova2007@abv.bg>
2023-01-31 18:16:40 +0200

    Update

---
+++ Python Fundamentals/Day 4 - lab (Functions)/06_calculate_rectangle_area.py
@@ -0,0 +1,22 @@
+#1
+
+def area(first, second):
+    return first * second
+
+
+a = int(input())
+b = int(input())
+print(area(a, b))
+
+
+
+
+#2
+
+
+area = lambda width, height: width * height
+
+
+a = int(input())
+b = int(input())
+print(area(a, b))
