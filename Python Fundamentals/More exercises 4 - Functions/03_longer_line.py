fbe5bfc038068a23337004fe63c3ea4ab4f0bafa
rayapetkova <rayapetkova2007@abv.bg>
2023-02-04 20:13:42 +0200

    Update

--- Python Fundamentals/More exercises 4 - Functions/03_longer_line.py
+++ Python Fundamentals/More exercises 4 - Functions/03_longer_line.py
@@ -1,33 +1,30 @@
 from math import floor
 
 
-def longer_line(x1, y1, x2, y2, x3, y3, x4, y4):
-    if abs(x1) + abs(y1) + abs(x2) + abs(y2) > abs(x3) + abs(y3) + abs(x4) + abs(y4):
-        closest_1 = abs(x1) + abs(y1)
-        closest_2 = abs(x2) + abs(y2)
-        if closest_1 < closest_2:
-            return f"({x1}, {y1})({x2}, {y2})"
+def longer_line(x1_n, y1_n, x2_n, y2_n, x3_n, y3_n, x4_n, y4_n):
+    x1, y1, x2, y2 = abs(x1_n), abs(y1_n), abs(x2_n), abs(y2_n)
+    x3, y3, x4, y4 = abs(x3_n), abs(y3_n), abs(x4_n), abs(y4_n)
+
+    if floor(x1 + y1 + x2 + y2) >= floor(x3 + y3 + x4 + y4):
+        closest_1 = x1 + y1
+        closest_2 = x2 + y2
+        if closest_1 <= closest_2:
+            return f"({x1_n}, {y1_n})({x2_n}, {y2_n})"
         if closest_1 > closest_2:
-            return f"({x2}, {y2})({x1}, {y1})"
-    elif abs(x1) + abs(y1) + abs(x2) + abs(y2) < abs(x3) + abs(y3) + abs(x4) + abs(y4):
-        closest_1 = abs(x3) + abs(y3)
-        closest_2 = abs(x4) + abs(y4)
-        if closest_1 < closest_2:
-            return f"({x3}, {y3})({x4}, {y4})"
+            return f"({x2_n}, {y2_n})({x1_n}, {y1_n})"
+
+    elif floor(x1 + y1 + x2 + y2) < floor(x3 + y3 + x4 + y4):
+        closest_1 = x3 + y3
+        closest_2 = x4 + y4
+        if closest_1 <= closest_2:
+            return f"({x3_n}, {y3_n})({x4_n}, {y4_n})"
         if closest_1 > closest_2:
-            return f"({x4}, {y4})({x3}, {y3})"
-    elif abs(x1) + abs(y1) + abs(x2) + abs(y2) == abs(x3) + abs(y3) + abs(x4) + abs(y4):
-        closest_1 = abs(x1) + abs(y1)
-        closest_2 = abs(x2) + abs(y2)
-        if closest_1 < closest_2:
-            return f"({x1}, {y1})({x2}, {y2})"
-        if closest_1 > closest_2:
-            return f"({x2}, {y2})({x1}, {y1})"
+            return f"({x4_n}, {y4_n})({x3_n}, {y3_n})"
 
 
-curr_x1, curr_y1 = float(input()), float(input())
-curr_x2, curr_y2 = float(input()), float(input())
-curr_x3, curr_y3 = float(input()), float(input())
-curr_x4, curr_y4 = float(input()), float(input())
+curr_x1, curr_y1 = floor(float(input())), floor(float(input()))
+curr_x2, curr_y2 = floor(float(input())), floor(float(input()))
+curr_x3, curr_y3 = floor(float(input())), floor(float(input()))
+curr_x4, curr_y4 = floor(float(input())), floor(float(input()))
 
 print(longer_line(curr_x1, curr_y1, curr_x2, curr_y2, curr_x3, curr_y3, curr_x4, curr_y4))
