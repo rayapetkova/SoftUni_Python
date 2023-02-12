d01642aa0db99fe01e07db859692e50d4b08cc3a
rayapetkova <rayapetkova2007@abv.bg>
2023-02-12 19:43:40 +0200

    Update

--- Python Fundamentals/MID EXAMS/02. Programming Fundamentals Mid Exam/03_numbers.py
+++ Python Fundamentals/MID EXAMS/02. Programming Fundamentals Mid Exam/03_numbers.py
@@ -3,9 +3,13 @@ numbers = [int(j) for j in input().split()]
 average = sum(numbers) / len(numbers)
 
 final = [j for j in numbers if j > average]
-reversed_list = list(reversed(final))
 
 if len(final) >= 1:
-    print(*sorted(reversed_list[0:5], reverse=True))
+    print(*sorted(final, reverse=True)[0:5])
 else:
     print("No")
+
+
+
+# 2
+
