299c4034c75d9c007e446084a7a3543161f5844e
rayapetkova <rayapetkova2007@abv.bg>
2023-02-12 19:47:40 +0200

    Update

--- Python Fundamentals/MID EXAMS/02. Programming Fundamentals Mid Exam/03_numbers.py
+++ Python Fundamentals/MID EXAMS/02. Programming Fundamentals Mid Exam/03_numbers.py
@@ -11,5 +11,18 @@ else:
 
 
 
-# 2
 
+
+# 2
+#
+# numbers = [int(j) for j in input().split()]
+#
+# average = sum(numbers) / len(numbers)
+#
+# final = [j for j in numbers if j > average]
+#
+# if len(final) >= 1:
+#     sorted_final = sorted(final, reverse=True)
+#     print(*sorted_final[0:5])
+# else:
+#     print("No")
