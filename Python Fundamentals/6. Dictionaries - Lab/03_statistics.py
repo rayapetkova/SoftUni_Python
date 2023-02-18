e7166b32c79391d58f5dd3fd84ad1db61253b66e
rayapetkova <rayapetkova2007@abv.bg>
2023-02-18 12:52:29 +0200

    Update 03. task

---
+++ Python Fundamentals/6. Dictionaries - Lab/03_statistics.py
@@ -0,0 +1,19 @@
+dictionary = {}
+
+while True:
+    line = input()
+    if line == "statistics":
+        break
+    product, quantity = line.split(": ")
+
+    if product not in dictionary:
+        dictionary[product] = 0
+
+    dictionary[product] += int(quantity)
+
+print(f"Products in stock:")
+
+[print(f"- {key}: {value}") for key, value in dictionary.items()]
+
+print(f"Total Products: {len(dictionary.keys())}")
+print(f"Total Quantity: {sum(dictionary.values())}")
