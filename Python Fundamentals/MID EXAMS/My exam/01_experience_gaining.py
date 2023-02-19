22c6e3d47e773fbe3cd3865ba4b8559c42bc8186
rayapetkova <rayapetkova2007@abv.bg>
2023-02-19 15:50:10 +0200

    Update 01. task

---
+++ Python Fundamentals/MID EXAMS/My exam/01_experience_gaining.py
@@ -0,0 +1,24 @@
+needed_experience = float(input())
+battles = int(input())
+
+counter = 0
+total = 0
+success = False
+
+for i in range(1, battles + 1):
+    experience = float(input())
+    total += experience
+    counter += 1
+    if i % 3 == 0:
+        total += (15 / 100) * experience
+    if i % 5 == 0:
+        total -= (10 / 100) * experience
+    if i % 15 == 0:
+        total += (5 / 100) * experience
+    if total >= needed_experience:
+        success = True
+        print(f"Player successfully collected his needed experience for {counter} battles.")
+        break
+
+if not success:
+    print(f"Player was not able to collect the needed experience, {(needed_experience - total):.2f} more needed.")
