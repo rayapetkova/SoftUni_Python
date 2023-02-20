55a3f9c516c0551be2ace9a8b57ff96f11a57668
rayapetkova <rayapetkova2007@abv.bg>
2023-02-21 00:40:32 +0200

    Update 01. task

--- Python Fundamentals/6. Dictionaries - Exercise/01_count_chars_in_a_string.py
+++ Python Fundamentals/6. Dictionaries - Exercise/01_count_chars_in_a_string.py
@@ -1,6 +1,6 @@
 text = input()
 
-dictionary = {element: text.count(element) for element in text if element != " "}
+dictionary = {element: text.count(element) for element in text if not element.isspace()}
 
 for key, value in dictionary.items():
     print(f"{key} -> {value}")
@@ -14,7 +14,22 @@ for key, value in dictionary.items():
 # dictionary = {}
 #
 # for element in text:
-#     if element != " ":
+#     if not element.isspace():
+#         dictionary[element] = dictionary.get(element, 0) + 1
+# for key, value in dictionary.items():
+#     print(f"{key} -> {value}")
+
+
+
+
+
+#3
+#
+# text = input()
+# dictionary = {}
+#
+# for element in text:
+#     if not element.isspace() and element not in dictionary.keys():
 #         dictionary[element] = text.count(element)
 #
 # for key, value in dictionary.items():
