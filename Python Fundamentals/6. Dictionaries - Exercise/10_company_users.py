771cf1da1b0d0924c73a6374db21102c392c7d92
rayapetkova <rayapetkova2007@abv.bg>
2023-02-23 18:02:28 +0200

    Update 10. task

--- Python Fundamentals/6. Dictionaries - Exercise/10_company_users.py
+++ Python Fundamentals/6. Dictionaries - Exercise/10_company_users.py
@@ -1,5 +1,3 @@
-from collections import OrderedDict
-
 companies = {}
 
 while True:
@@ -7,9 +5,31 @@ while True:
     if line == "End":
         break
     company_name, company_id = line.split(" -> ")
-    companies[company_name] = companies.get(company_name, []) + [company_id]
+    companies[company_name] = companies.get(company_name, [])
+    companies[company_name].append(company_id) if company_id not in companies[company_name] else None
 
 for name, all_id in companies.items():
     print(name)
-    for id in list(OrderedDict.fromkeys(all_id)):
+    for id in all_id:
         print(f"-- {id}")
+
+
+
+
+#2
+#
+# from collections import OrderedDict
+#
+# companies = {}
+#
+# while True:
+#     line = input()
+#     if line == "End":
+#         break
+#     company_name, company_id = line.split(" -> ")
+#     companies[company_name] = companies.get(company_name, []) + [company_id]
+#
+# for name, all_id in companies.items():
+#     print(name)
+#     for id in list(OrderedDict.fromkeys(all_id)):
+#         print(f"-- {id}")
