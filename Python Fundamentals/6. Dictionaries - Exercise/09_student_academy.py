424186158e98985dc910d21c953b9d20b36d7769
rayapetkova <rayapetkova2007@abv.bg>
2023-02-23 16:37:10 +0200

    Update 09. task

--- Python Fundamentals/6. Dictionaries - Exercise/09_student_academy.py
+++ Python Fundamentals/6. Dictionaries - Exercise/09_student_academy.py
@@ -6,5 +6,4 @@ for _ in range(num):
     student_grade = float(input())
     good_students[student_name] = good_students.get(student_name, []) + [student_grade]
 
-
 [print(f"{name} -> {(sum(grade) / len(grade)):.2f}") for name, grade in good_students.items() if (sum(grade) / len(grade)) >= 4.50]
