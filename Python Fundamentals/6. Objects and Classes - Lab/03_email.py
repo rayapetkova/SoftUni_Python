32d23ca139721aaf6c0ed7c668f86a4cad7fd429
rayapetkova <rayapetkova2007@abv.bg>
2023-02-24 18:01:29 +0200

    Update 03.task

---
+++ Python Fundamentals/6. Objects and Classes - Lab/03_email.py
@@ -0,0 +1,32 @@
+class Email:
+    def __init__(self, sender, receiver, content):
+        self.sender = sender
+        self.receiver = receiver
+        self.content = content
+        self.is_sent = False
+
+    def send(self):
+        self.is_sent = True
+
+    def get_info(self):
+        return f"{self.sender} says to {self.receiver}: {self.content}. Sent: {self.is_sent}"
+
+
+all_emails = []
+
+while True:
+    command = input()
+    if command == "Stop":
+        break
+    line = command.split()
+    message_sender, message_receiver, message = line[0], line[1], line[2]
+    email = Email(message_sender, message_receiver, message)
+    all_emails.append(email)
+
+indices = list(map(int, input().split(", ")))
+
+for idx in indices:
+    all_emails[idx].send()
+
+for some_email in all_emails:
+    print(some_email.get_info())
