d5d6581b8c93a2174c331d01facda35f02bfe9ff
rayapetkova <rayapetkova2007@abv.bg>
2023-02-25 01:53:39 +0200

    Update 06. task
    Update 06. task

---
+++ Python Fundamentals/6. Objects and Classes - Exericse/06_inventory.py
@@ -0,0 +1,29 @@
+class Inventory:
+    __some = 0
+
+    def __init__(self, capacity):
+        self.__capacity = capacity
+        self.items = []
+
+    def add_item(self, item):
+        if len(self.items) < self.__capacity:
+            self.items.append(item)
+            Inventory.__some += 1
+        else:
+            return f"not enough room in the inventory"
+
+    def get_capacity(self):
+        return self.__capacity
+
+    def __repr__(self):
+        return f"Items: {', '.join(self.items)}.\nCapacity left: {self.get_capacity() - Inventory.__some}"
+
+
+# This is a test code:
+#
+# inventory = Inventory(2)
+# inventory.add_item("potion")
+# inventory.add_item("sword")
+# print(inventory.add_item("bottle"))
+# print(inventory.get_capacity())
+# print(inventory)
