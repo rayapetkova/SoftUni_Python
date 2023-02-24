aef14445f789d801f4afab7bf0c24b68a22ff0ca
rayapetkova <rayapetkova2007@abv.bg>
2023-02-24 18:03:56 +0200

    Update 03.task

--- Python Fundamentals/6. Objects and Classes - Lab/03_email.py
+++ Python Fundamentals/6. Objects and Classes - Lab/03_email.py
@@ -1,9 +1,10 @@
 class Email:
+    is_sent = False
+
     def __init__(self, sender, receiver, content):
         self.sender = sender
         self.receiver = receiver
         self.content = content
-        self.is_sent = False
 
     def send(self):
         self.is_sent = True
