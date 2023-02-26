26c678e8e8aa9579cd817e1254becbc2d8df22cc
rayapetkova <rayapetkova2007@abv.bg>
2023-02-26 22:32:32 +0200

    Update 01. task

---
+++ Python Fundamentals/8. Text Processing - Exercise/01_valid_usernames.py
@@ -0,0 +1,11 @@
+usernames = input().split(", ")
+
+for username in usernames:
+    if 3 <= len(username) <= 16:
+        not_valid = False
+        for el in username:
+            if not el.isalpha() and not el.isdigit() and el != "-" and el != "_":
+                not_valid = True
+                break
+        if not not_valid:
+            print(username)
