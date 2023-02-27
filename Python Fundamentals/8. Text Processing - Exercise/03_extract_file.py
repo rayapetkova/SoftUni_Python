09ad134788bbbc17377e3eb544252f67e3d2e351
rayapetkova <rayapetkova2007@abv.bg>
2023-02-27 16:00:47 +0200

    Update 03. task

---
+++ Python Fundamentals/8. Text Processing - Exercise/03_extract_file.py
@@ -0,0 +1,10 @@
+path = input().split("\\")
+file = path[-1].split(".")
+print(f"File name: {file[0]}\nFile extension: {file[1]}")
+
+
+
+
+#2
+# file = input().split("\\")[-1].split(".")
+# print(f"File name: {file[0]}\nFile extension: {file[1]}")
