ca6ecc4a1f8a3bd7786b8aa2730cfc929cd759ae
rayapetkova <rayapetkova2007@abv.bg>
2023-02-28 23:56:34 +0200

    Update 03. task

--- Python Fundamentals/7. Dictionaries - More Exercises/03_MOBA_challenger.py
+++ Python Fundamentals/7. Dictionaries - More Exercises/03_MOBA_challenger.py
@@ -8,32 +8,39 @@ pool = {}
 # Example: {"Simon": 450, "Frank": 350, "Peter": 200}
 final = {}
 
+
+def add_position_and_skill(curr_pool, curr_player, curr_position):
+    curr_pool[curr_player] = curr_pool.get(curr_player, {})
+    curr_pool[curr_player][curr_position] = curr_pool[curr_player].get(curr_position, 0)
+
+
+def check_greater_skill(curr_skill, curr_second):
+    return curr_skill > curr_second
+
+
+def duel(curr_first_player, curr_second_player, curr_pool):
+    for first in curr_pool[curr_first_player].keys():
+        for second in curr_pool[curr_second_player].keys():
+            if first == second:
+                if check_greater_skill(curr_pool[curr_first_player][first], curr_pool[curr_second_player][second]):
+                    curr_pool[curr_second_player][second] = 0
+                if check_greater_skill(curr_pool[curr_second_player][second], curr_pool[curr_first_player][first]):
+                    curr_pool[curr_first_player][first] = 0
+
+
 while True:
     line = input()
     if line == "Season end":
         break
     if "->" in line:
         player, position, skill = [int(element) if element.isdigit() else element for element in line.split(" -> ")]
-        if player not in pool.keys():
-            pool[player] = {position: skill}
-        else:
-            if position in pool[player]:
-                if skill > pool[player][position]:
-                    pool[player][position] = skill
-            else:
-                pool[player] = pool.get(player, {})
-                pool[player][position] = pool[player].get(position, skill)
+        add_position_and_skill(pool, player, position)
+        if check_greater_skill(skill, pool[player][position]):
+            pool[player][position] = skill
     else:
-        command = line.split(" vs ")
-        first_player, second_player = command[0], command[1]
+        first_player, second_player = line.split(" vs ")
         if first_player in pool.keys() and second_player in pool.keys():
-            for first in pool[first_player].keys():
-                for second in pool[second_player].keys():
-                    if first == second:
-                        if pool[first_player][first] > pool[second_player][second]:
-                            pool[second_player][second] = 0
-                        elif pool[second_player][second] > pool[first_player][first]:
-                            pool[first_player][first] = 0
+            duel(first_player, second_player, pool)
 
 for key, value in pool.items():
     final[key] = sum(value.values())
@@ -43,3 +50,47 @@ for name, total in sorted(final.items(), key=lambda x: (-x[1], x[0])):
         print(f"{name}: {total} skill")
         for second_key, second_value in sorted(pool[name].items(), key=lambda x: (-x[1], x[0])):
             print(f"- {second_key} <::> {second_value}")
+
+
+
+
+
+#2
+#
+# pool = {}
+# # {"George": {Jungle: 250}}
+# # {player: {position: skill}}
+#
+# final = {}
+#
+# while True:
+#     line = input()
+#     if line == "Season end":
+#         break
+#     if "->" in line:
+#         player, position, skill = [int(element) if element.isdigit() else element for element in line.split(" -> ")]
+#         pool[player] = pool.get(player, {})
+#         pool[player][position] = pool[player].get(position, 0)
+#         if skill > pool[player][position]:
+#             pool[player][position] = skill
+#     else:
+#         command = line.split(" vs ")
+#         first_player, second_player = command[0], command[1]
+#         if first_player in pool.keys() and second_player in pool.keys():
+#             for first in pool[first_player].keys():
+#                 for second in pool[second_player].keys():
+#                     if first == second:
+#                         if pool[first_player][first] > pool[second_player][second]:
+#                             pool[second_player][second] = 0
+#                         elif pool[second_player][second] > pool[first_player][first]:
+#                             pool[first_player][first] = 0
+#
+#
+# for key, value in pool.items():
+#     final[key] = sum(value.values())
+#
+# for name, total in sorted(final.items(), key=lambda x: (-x[1], x[0])):
+#     if total != 0:
+#         print(f"{name}: {total} skill")
+#         for second_key, second_value in sorted(pool[name].items(), key=lambda x: (-x[1], x[0])):
+#             print(f"- {second_key} <::> {second_value}")
