ebc449dc5cb6ff23bfcb147eb9ee9bfb824a45cd
rayapetkova <rayapetkova2007@abv.bg>
2023-03-06 01:10:53 +0200

    Update 03. task

--- Python Fundamentals/9. Regular Expressions - Lab/03_match_dates.py
+++ Python Fundamentals/9. Regular Expressions - Lab/03_match_dates.py
@@ -6,4 +6,19 @@ pattern = r"\b(?P<day>\d{2})([/.-])(?P<month>[A-Z][a-z]{2})\2(?P<year>\d{4})\b"
 valid_dates = re.finditer(pattern, all_dates)
 
 for date in valid_dates:
-    print(f"Day: {date.group('day')}, Month: {date.group('month')}, Year: {date.group('year')}")
+    print(f"Day: {date['day']}, Month: {date['month']}, Year: {date['year']}")
+
+
+
+
+#2
+#
+# import re
+#
+# all_dates = input()
+#
+# pattern = r"\b(\d{2})([/.-])([A-Z][a-z]{2})\2(\d{4})\b"
+# valid_dates = re.finditer(pattern, all_dates)
+#
+# for date in valid_dates:
+#     print(f"Day: {date[1]}, Month: {date[3]}, Year: {date[4]}")
