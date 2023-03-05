f9b151f1adc1993fe2796e315794d990cbd176c5
rayapetkova <rayapetkova2007@abv.bg>
2023-03-06 01:25:00 +0200

    Update 02. task

--- Python Fundamentals/9. Regular Expressions - Lab/02_match_phone_numbers.py
+++ Python Fundamentals/9. Regular Expressions - Lab/02_match_phone_numbers.py
@@ -1,10 +1,10 @@
 import re
 numbers = input()
 
-pattern = re.finditer(r"\+359([ -])2\1\d{3}\1\d{4}\b", numbers)
+matches = re.finditer(r"\+359([ -])2\1\d{3}\1\d{4}\b", numbers)
 
 valid_numbers = []
-for number in pattern:
+for number in matches:
     valid_numbers.append(number.group())
 
 print(", ".join(valid_numbers))
