f733ba11cecb68975c750b9b9f9f48ab4518195c
rayapetkova <rayapetkova2007@abv.bg>
2023-03-05 18:24:06 +0200

    Update 02. task

--- Python Fundamentals/9. Regular Expressions - Lab/02_match_phone_numbers.py
+++ Python Fundamentals/9. Regular Expressions - Lab/02_match_phone_numbers.py
@@ -1,7 +1,7 @@
 import re
 numbers = input()
 
-pattern = re.finditer(r"\+359([ -])\d\1\d{3}\1\d{4}\b", numbers)
+pattern = re.finditer(r"\+359([ -])2\1\d{3}\1\d{4}\b", numbers)
 
 valid_numbers = []
 for number in pattern:
