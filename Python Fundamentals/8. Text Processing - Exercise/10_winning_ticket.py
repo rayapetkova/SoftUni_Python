794f3fa87b02a1ce2cd89103c6453f663a5a526e
rayapetkova <rayapetkova2007@abv.bg>
2023-03-06 18:11:05 +0200

    Update 10. task

--- Python Fundamentals/8. Text Processing - Exercise/10_winning_ticket.py
+++ Python Fundamentals/8. Text Processing - Exercise/10_winning_ticket.py
@@ -1,60 +1,101 @@
 tickets = input().split()
 
-def check(first, second, symbol):
-    total_first = 0
-    first_list = []
-    for i in range(len(first)):
-        if first[i] == symbol:
-            total_first += 1
-            if i == len(first) - 1:
-                first_list.append(total_first)
-        else:
-            first_list.append(total_first)
-            total_first = 0
 
-    total_second = 0
-    second_list = []
-    for i in range(len(second)):
-        if second[i] == symbol:
-            total_second += 1
-            if i == len(second) - 1:
-                second_list.append(total_second)
+def adding_to_list(half, curr_symbol):
+    total = 0
+    half_symbols = []
+    for i in range(len(half)):
+        if half[i] == curr_symbol:
+            total += 1
+            if i == len(half) - 1:
+                half_symbols.append(total)
         else:
-            second_list.append(total_second)
-            total_second = 0
+            half_symbols.append(total)
+            total = 0
+    return max(half_symbols)
 
-    max_first = max(first_list)
-    max_second = max(second_list)
-    if max_first < max_second:
+
+def final_result(first, second, symbol):
+    max_first, max_second = adding_to_list(first, symbol), adding_to_list(second, symbol)
+    if adding_to_list(first, symbol) < adding_to_list(second, symbol):
         return f'ticket "{first + second}" - {max_first}{symbol}'
-    else:
-        return f'ticket "{first + second}" - {max_second}{symbol}'
+    return f'ticket "{first + second}" - {max_second}{symbol}'
 
 
 for ticket in tickets:
-    if "," in ticket:
-        ticket = ticket.replace(",", "")
+    ticket = ticket.replace(",", "")
+    half = int(len(ticket) / 2)
     if ticket == "":
         continue
-    half = int(len(ticket) / 2)
     if len(ticket) == 20:
-        first_half = ticket[0:half]
-        second_half = ticket[half:len(ticket)]
-        if "@" * 20 in ticket or "#" * 20 in ticket or "$" * 20 in ticket or "^" * 20 in ticket:
+        first_half = ticket[:half]
+        second_half = ticket[half:]
+        if any(["@" * 20 in ticket, "#" * 20 in ticket, "$" * 20 in ticket, "^" * 20 in ticket]):
             print(f'ticket "{ticket}" - 10{ticket[0]} Jackpot!')
-        elif "@" * 6 in first_half and "@" * 6 in second_half:
-            curr_symbol = "@"
-            print(check(first_half, second_half, curr_symbol))
-        elif "#" * 6 in first_half and "#" * 6 in second_half:
-            curr_symbol = "#"
-            print(check(first_half, second_half, curr_symbol))
-        elif "$" * 6 in first_half and "$" * 6 in second_half:
-            curr_symbol = "$"
-            print(check(first_half, second_half, curr_symbol))
-        elif "^" * 6 in first_half and "^" * 6 in second_half:
-            curr_symbol = "^"
-            print(check(first_half, second_half, curr_symbol))
+        elif all(["@" * 6 in first_half, "@" * 6 in second_half]):
+            print(final_result(first_half, second_half, "@"))
+        elif all(["#" * 6 in first_half, "#" * 6 in second_half]):
+            print(final_result(first_half, second_half, "#"))
+        elif all(["$" * 6 in first_half, "$" * 6 in second_half]):
+            print(final_result(first_half, second_half, "$"))
+        elif all(["^" * 6 in first_half, "^" * 6 in second_half]):
+            print(final_result(first_half, second_half, "^"))
         else:
             print(f'ticket "{ticket}" - no match')
     else:
         print(f"invalid ticket")
+
+
+
+
+
+
+#2
+#
+# tickets = input().split()
+# symbols = {'at_symbol': ("@" * 6), 'pound_symbol': ("#" * 6), 'dollar_symbol': ("$" * 6), 'circumflex_symbol': ("^" * 6)}
+#
+#
+# def adding_to_list(half, curr_symbol):
+#     total = 0
+#     half_symbols = []
+#     for i in range(len(half)):
+#         if half[i] == curr_symbol:
+#             total += 1
+#             if i == len(half) - 1:
+#                 half_symbols.append(total)
+#         else:
+#             half_symbols.append(total)
+#             total = 0
+#     return max(half_symbols)
+#
+#
+# def final_result(first, second, symbol):
+#     max_first, max_second = adding_to_list(first, symbol), adding_to_list(second, symbol)
+#     if adding_to_list(first, symbol) < adding_to_list(second, symbol):
+#         return f'ticket "{first + second}" - {max_first}{symbol}'
+#     return f'ticket "{first + second}" - {max_second}{symbol}'
+#
+#
+# for ticket in tickets:
+#     ticket = ticket.replace(",", "")
+#     half = int(len(ticket) / 2)
+#     if ticket == "":
+#         continue
+#     if len(ticket) == 20:
+#         first_half = ticket[:half]
+#         second_half = ticket[half:]
+#         if any(["@" * 20 in ticket, "#" * 20 in ticket, "$" * 20 in ticket, "^" * 20 in ticket]):
+#             print(f'ticket "{ticket}" - 10{ticket[0]} Jackpot!')
+#         elif all([symbols['at_symbol'] in first_half, symbols['at_symbol'] in second_half]):
+#             print(final_result(first_half, second_half, "@"))
+#         elif all([symbols['pound_symbol'] in first_half, symbols['pound_symbol'] in second_half]):
+#             print(final_result(first_half, second_half, "#"))
+#         elif all([symbols['dollar_symbol'] in first_half, symbols['dollar_symbol'] in second_half]):
+#             print(final_result(first_half, second_half, "$"))
+#         elif all([symbols['circumflex_symbol'] in first_half, symbols['circumflex_symbol'] in second_half]):
+#             print(final_result(first_half, second_half, "^"))
+#         else:
+#             print(f'ticket "{ticket}" - no match')
+#     else:
+#         print(f"invalid ticket")
