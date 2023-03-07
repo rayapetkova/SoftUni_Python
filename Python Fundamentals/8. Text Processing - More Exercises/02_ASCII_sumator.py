49064b48ac4d806f66fae807462339a8d35bd1be
rayapetkova <rayapetkova2007@abv.bg>
2023-03-07 21:25:27 +0200

    Update 02. task

---
+++ Python Fundamentals/8. Text Processing - More Exercises/02_ASCII_sumator.py
@@ -0,0 +1,16 @@
+first_symbol, second_symbol,  text = ord(input()), ord(input()), input()
+total = 0
+
+for element in text:
+    if first_symbol < ord(element) < second_symbol:
+        total += ord(element)
+
+print(total)
+
+
+
+
+#2
+#
+# first_symbol, second_symbol,  text = ord(input()), ord(input()), input()
+# total = print(sum([ord(element) for element in text if first_symbol < ord(element) < second_symbol]))
