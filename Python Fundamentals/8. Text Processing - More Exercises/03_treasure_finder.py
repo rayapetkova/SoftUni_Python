ae36c79094bc528961585995fd3c220f7e21b4d6
rayapetkova <rayapetkova2007@abv.bg>
2023-03-08 15:28:51 +0200

    Update 03. task

--- Python Fundamentals/8. Text Processing - More Exercises/03_treasure_finder.py
+++ Python Fundamentals/8. Text Processing - More Exercises/03_treasure_finder.py
@@ -1,9 +1,9 @@
 key = list(map(int, input().split()))
 
 
-def find_items(curr_line, symbol):
-    start = curr_line.find(symbol)
-    end = curr_line.find(symbol, start + 1)
+def find_items(curr_line, first_symbol, second_symbol):
+    start = curr_line.find(first_symbol)
+    end = curr_line.find(second_symbol, start + 1)
     return curr_line[start + 1:end]
 
 
@@ -16,8 +16,10 @@ while True:
         num = key[i % len(key)]
         line[i] = chr(ord(line[i]) - num)
     final_line = "".join(line)
-    type_treasure = find_items(final_line, "&")
 
-    start_coordinates, end_coordinates = final_line.find("<"), final_line.find(">")
-    coordinates = final_line[start_coordinates + 1:end_coordinates]
+    treasure_symbol = "&"
+    start_coordinates_symbol = "<"
+    end_coordinates_symbol = ">"
+    type_treasure = find_items(final_line, treasure_symbol, treasure_symbol)
+    coordinates = find_items(final_line, start_coordinates_symbol, end_coordinates_symbol)
     print(f"Found {type_treasure} at {coordinates}")
