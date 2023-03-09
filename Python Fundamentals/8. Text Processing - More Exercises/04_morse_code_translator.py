07070b2cd4548bd30cc57aae6a5ffa66ae70118b
rayapetkova <rayapetkova2007@abv.bg>
2023-03-09 16:33:43 +0200

    Update 04. task

--- Python Fundamentals/8. Text Processing - More Exercises/04_morse_code_translator.py
+++ Python Fundamentals/8. Text Processing - More Exercises/04_morse_code_translator.py
@@ -9,12 +9,39 @@ morse_code = {'A': '.-', 'B': '-...',
               'X': '-..-', 'Y': '-.--', 'Z': '--..'}
 
 words = input().split(" | ")
-final = []
+final = ""
 
 for word in words:
     morse_code_letters = word.split()
     for morse_code_letter in morse_code_letters:
-        [final.append(key) for key in morse_code if morse_code[key] == morse_code_letter]
-    final.append(" ")
+        final += " ".join(key for key in morse_code if morse_code[key] == morse_code_letter)
+    final += " "
 
 print(*final, sep="")
+
+
+
+
+
+#2 - a solution with list:
+#
+# morse_code = {'A': '.-', 'B': '-...',
+#               'C': '-.-.', 'D': '-..', 'E': '.',
+#               'F': '..-.', 'G': '--.', 'H': '....',
+#               'I': '..', 'J': '.---', 'K': '-.-',
+#               'L': '.-..', 'M': '--', 'N': '-.',
+#               'O': '---', 'P': '.--.', 'Q': '--.-',
+#               'R': '.-.', 'S': '...', 'T': '-',
+#               'U': '..-', 'V': '...-', 'W': '.--',
+#               'X': '-..-', 'Y': '-.--', 'Z': '--..'}
+#
+# words = input().split(" | ")
+# final = []
+#
+# for word in words:
+#     morse_code_letters = word.split()
+#     for morse_code_letter in morse_code_letters:
+#         [final.append(key) for key in morse_code if morse_code[key] == morse_code_letter]
+#     final.append(" ")
+#
+# print(*final, sep="")
