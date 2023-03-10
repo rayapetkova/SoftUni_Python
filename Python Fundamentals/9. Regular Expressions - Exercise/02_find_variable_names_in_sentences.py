34c783e56e421a9460f68ca18052adf4da4b13eb
rayapetkova <rayapetkova2007@abv.bg>
2023-03-10 15:12:20 +0200

    Update 02. task

--- Python Fundamentals/9. Regular Expressions - Exercise/02_find_variable_names_in_sentences.py
+++ Python Fundamentals/9. Regular Expressions - Exercise/02_find_variable_names_in_sentences.py
@@ -10,3 +10,22 @@ for name in valid_names:
     final.append(name[1])
 
 print(*final, sep=",")
+
+
+
+
+
+#2
+#
+# import re
+#
+# names = input()
+#
+# pattern = r"\b_(?P<name>[A-Za-z0-9]+)\b"
+# valid_names = re.finditer(pattern, names)
+#
+# final = []
+# for name in valid_names:
+#     final.append(name["name"])
+#
+# print(*final, sep=",")
