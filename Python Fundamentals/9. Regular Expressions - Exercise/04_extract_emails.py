0849a0992af5c33c4253aaabca46699e8522140a
rayapetkova <rayapetkova2007@abv.bg>
2023-03-11 19:33:09 +0200

    Update 04. task

--- Python Fundamentals/9. Regular Expressions - Exercise/04_extract_emails.py
+++ Python Fundamentals/9. Regular Expressions - Exercise/04_extract_emails.py
@@ -2,7 +2,7 @@ import re
 
 text = input()
 
-pattern = r"(^|\s)(?P<email>[a-zA-Z0-9]+[\.\-\_]*[a-zA-Z0-9]*@[a-zA-Z\-]+[a-zA-Z\-]\.[\.a-zA-z]*[a-zA-Z])"
+pattern = r"(^|\s)(?P<email>[a-zA-Z\d]+[\.\-\_]*[a-zA-Z\d]*@[a-zA-Z]+[\-]*[a-zA-Z]*\.[\.a-zA-z]*[a-zA-Z])"
 valid_emails = re.finditer(pattern, text)
 
 for valid_email in valid_emails:
