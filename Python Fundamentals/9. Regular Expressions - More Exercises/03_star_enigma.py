01ab31a5d398dfce399f5fc7ef8ba9a7fd066efa
rayapetkova <rayapetkova2007@abv.bg>
2023-03-12 14:05:20 +0200

    Update 03. task

---
+++ Python Fundamentals/9. Regular Expressions - More Exercises/03_star_enigma.py
@@ -0,0 +1,32 @@
+import re
+
+dictionary = {"attacked": [], "destroyed": []}
+attacked = 0
+destroyed = 0
+number = int(input())
+
+pattern = r"@(?P<planet>[a-zA-Z]+)[^@\-\!\>\:]*:(?P<population>\d+)[^@\-\!\>\:]*!(?P<type>[A|D])![^@\-\!\>\:]*->(?P<soldiers>\d+)"
+
+for i in range(number):
+    text = input()
+    counter = text.upper().count("S") + text.upper().count("T") + text.upper().count("A") + text.upper().count("R")
+    text = list(text)
+    for idx in range(len(text)):
+        text[idx] = chr(ord(text[idx]) - counter)
+    message = "".join(text)
+    valid_message = re.finditer(pattern, message)
+    for some_message in valid_message:
+        if some_message["type"] == "A":
+            attacked += 1
+            dictionary["attacked"].append(some_message["planet"])
+        elif some_message["type"] == "D":
+            destroyed += 1
+            dictionary["destroyed"].append(some_message["planet"])
+
+sorted_attacked_dict = sorted(dictionary["attacked"])
+print(f"Attacked planets: {attacked}")
+[print(f"-> {attacked_planet}") for attacked_planet in sorted_attacked_dict]
+
+sorted_destroyed_dict = sorted(dictionary["destroyed"])
+print(f"Destroyed planets: {destroyed}")
+[print(f"-> {destroyed_planet}") for destroyed_planet in sorted_destroyed_dict]
