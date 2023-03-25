23a0be44cf8b14693c9aba0c45f5520b578a5ee5
rayapetkova <rayapetkova2007@abv.bg>
2023-03-25 19:36:24 +0200

    Update 03. task

---
+++ Python Fundamentals/Python Fundamentals - Exercises (Open Courses)/Python Intro/03_extended_person_info.py
@@ -0,0 +1,18 @@
+name, age, town, salary = input(), int(input()), input(), float(input())
+age_range, salary_range = "", ""
+
+if age < 18:
+    age_range = "teen"
+elif age < 70:
+    age_range = "adult"
+else:
+    age_range = "elder"
+
+if salary < 500:
+    salary_range = "low"
+elif salary < 2000:
+    salary_range = "medium"
+else:
+    salary_range = "high"
+
+print(f"Name: {name}\nAge: {age}\nTown: {town}\nSalary: ${salary:.2f}\nAge range: {age_range}\nSalary range: {salary_range}")
