ced9550cf9148194b1a578b96fbc935394282013
rayapetkova <rayapetkova2007@abv.bg>
2023-04-01 22:46:04 +0300

    Update

--- Python Fundamentals/More Exercises - Dictionaries and Lists (Archive)/01_sort_times.py
+++
@@ -1,4 +0,0 @@
-times = input().split()
-
-sorted_times = sorted(times)
-print(*sorted_times, sep=", ")
--- Python Fundamentals/More Exercises - Dictionaries and Lists (Archive)/02_odd_filter.py
+++
@@ -1,7 +0,0 @@
-# final = []
-
-numbers = [int(i) for i in list(map(int, input().split())) if i % 2 == 0]
-average = sum(numbers) / len(numbers)
-final = [num + 1 if num > average else num - 1 for num in numbers]
-
-print(*final, sep=" ")
