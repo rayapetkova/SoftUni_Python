e25191fc59c754adcd4bf39505700d4304805c12
rayapetkova <rayapetkova2007@abv.bg>
2023-04-02 10:11:52 +0300

    Update 03. task

---
+++ Python Fundamentals/FINAL EXAMS/My exam/03_degustation_party.py
@@ -0,0 +1,33 @@
+dictionary = {}
+count_unliked_meals = 0
+
+while True:
+    line = input()
+    if line == "Stop":
+        break
+    command = line.split("-")
+    guest, meal = command[1], command[2]
+    if "Like" in command:
+        if guest not in dictionary.keys():
+            dictionary[guest] = []
+        if meal not in dictionary[guest]:
+            dictionary[guest].append(meal)
+    elif "Dislike" in command:
+        if guest in dictionary.keys():
+            if meal in dictionary[guest]:
+                print(f"{guest} doesn't like the {meal}.")
+                lst = [something for something in dictionary[guest] if something != meal]
+                dictionary[guest] = lst
+                count_unliked_meals += 1
+            else:
+                print(f"{guest} doesn't have the {meal} in his/her collection.")
+        else:
+            print(f"{guest} is not at the party.")
+
+for key, value in dictionary.items():
+    if not value:
+        print(f"{key}: ")
+    if value:
+        print(f"{key}: {', '.join(value)}")
+
+print(f"Unliked meals: {count_unliked_meals}")
