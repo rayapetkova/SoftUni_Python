f6e6cef048b808a6da2b99901a1c05b9da3eabbb
rayapetkova <rayapetkova2007@abv.bg>
2023-04-02 09:28:50 +0300

    Update 02. task

---
+++ Python Fundamentals/FINAL EXAMS/My exam/02_encrypting_password.py
@@ -0,0 +1,19 @@
+import re
+
+num = int(input())
+
+pattern = r"(.+)>(?P<password>\d{3}\|[a-z]{3}\|[A-Z]{3}\|[^<>]{3})<\1"
+
+for i in range(num):
+    line = input()
+    valid_passwords = re.finditer(pattern, line)
+    final = []
+    for valid in valid_passwords:
+        final.append(valid.group(2))
+    if final:
+        second = ""
+        for some in final:
+            second = some.replace("|", "")
+        print(f"Password: {second}")
+    else:
+        print(f"Try another password!")
