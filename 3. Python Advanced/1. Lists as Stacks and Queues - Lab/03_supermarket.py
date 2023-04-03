806f109d5dbf4145f6369890bce39a488d3db1b5
rayapetkova <rayapetkova2007@abv.bg>
2023-04-03 16:20:57 +0300

    Update 03. task

---
+++ 3. Python Advanced/1. Lists as Stacks and Queues - Lab/03_supermarket.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+people = deque()
+
+while True:
+    name = input()
+    if name == "End":
+        print(f"{len(people)} people remaining.")
+        break
+    if name == "Paid":
+        while people:
+            print(people.popleft())
+        continue
+    people.append(name)
