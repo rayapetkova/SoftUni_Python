66ce782f86158901387e38801a95555bdf6882be
rayapetkova <rayapetkova2007@abv.bg>
2023-04-03 16:32:36 +0300

    Update 04. task

---
+++ 3. Python Advanced/1. Lists as Stacks and Queues - Lab/04_water_dispenser.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+quantity = int(input())
+
+people = deque()
+
+while True:
+    name = input()
+    if name == "Start":
+        break
+    people.append(name)
+
+while True:
+    line = input()
+    if line == "End":
+        break
+    if line.isdigit():
+        liters = int(line)
+        if quantity >= liters:
+            print(f"{people.popleft()} got water")
+            quantity -= liters
+        else:
+            print(f"{people.popleft()} must wait")
+    else:
+        command, liters = line.split()[0], int(line.split()[1])
+        quantity += liters
+
+print(f"{quantity} liters left")
