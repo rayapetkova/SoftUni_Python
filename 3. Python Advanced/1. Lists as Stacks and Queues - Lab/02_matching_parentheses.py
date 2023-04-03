0910bbb95f18531e9c829ca4fb85dcbeaa75e445
rayapetkova <rayapetkova2007@abv.bg>
2023-04-03 16:05:10 +0300

    Update 02. task

---
+++ 3. Python Advanced/1. Lists as Stacks and Queues - Lab/02_matching_parentheses.py
@@ -0,0 +1,27 @@
+expression = input()
+stack_line = []
+
+for i in range(len(expression)):
+    if expression[i] == "(":
+        stack_line.append(i)
+    elif expression[i] == ")":
+        stack_line.append(i)
+        end = stack_line.pop()
+        start = stack_line.pop()
+        print(expression[start:end + 1])
+
+
+
+
+
+#2
+#
+# expression = input()
+# stack_line = []
+#
+# for i in range(len(expression)):
+#     if expression[i] == "(":
+#         stack_line.append(i)
+#     elif expression[i] == ")":
+#         start = stack_line.pop()
+#         print(expression[start:i + 1])
