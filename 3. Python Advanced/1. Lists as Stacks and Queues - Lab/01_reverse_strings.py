ed71ce32397bb524718b93d03786c2d029444b47
rayapetkova <rayapetkova2007@abv.bg>
2023-04-03 16:03:08 +0300

    Update 01. task

--- 3. Python Advanced/1. Lists as Stacks and Queues - Lab/01_reverse_strings.py
+++ 3. Python Advanced/1. Lists as Stacks and Queues - Lab/01_reverse_strings.py
@@ -1,7 +1,20 @@
 text = list(input())
 stack_line = []
 
-for el in range(len(text)):
+while text:
     stack_line.append(text.pop())
 
 print("".join(stack_line))
+
+
+
+
+#2
+#
+# text = list(input())
+# stack_line = []
+#
+# for el in range(len(text)):
+#     stack_line.append(text.pop())
+#
+# print("".join(stack_line))
