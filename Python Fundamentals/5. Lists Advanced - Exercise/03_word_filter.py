330662ce02c4d368766478ea0b94511729fa91ee
rayapetkova <rayapetkova2007@abv.bg>
2023-04-03 11:24:20 +0300

    Update 03. task

--- Python Fundamentals/5. Lists Advanced - Exercise/03_word_filter.py
+++ Python Fundamentals/5. Lists Advanced - Exercise/03_word_filter.py
@@ -1,12 +1,9 @@
 def word_filter(words):
-    for i in words:
-        if len(i) % 2 == 0:
-            return i
+    [print(word) for word in words if len(word) % 2 == 0]
 
 
 current_words = input().split()
-print(word_filter(current_words))
-
+word_filter(current_words)
 
 
 
