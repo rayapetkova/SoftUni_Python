ba41846bb6d2118b662f3be4d1ae39a3b3f17ff8
rayapetkova <rayapetkova2007@abv.bg>
2023-04-04 16:51:12 +0300

    Update 04. task

---
+++ 3. Python Advanced/2. Lists as Stacks and Queues - Exercise/04_fashion_boutique.py
@@ -0,0 +1,19 @@
+clothes_box_stack = list(map(int, input().split()))
+one_rack_capacity = int(input())
+sum_elements = 0
+racks = 0
+
+while clothes_box_stack:
+    element = clothes_box_stack.pop()
+    sum_elements += element
+    if sum_elements == one_rack_capacity:
+        racks += 1
+        sum_elements = 0
+    elif sum_elements > one_rack_capacity:
+        clothes_box_stack.append(element)
+        racks += 1
+        sum_elements = 0
+    if not clothes_box_stack and sum_elements > 0:
+        racks += 1
+
+print(racks)
