53269707735b6dce6561f4501a1009c4f464a375
rayapetkova <rayapetkova2007@abv.bg>
2023-04-04 15:10:57 +0300

    Update 03. task

---
+++ 3. Python Advanced/2. Lists as Stacks and Queues - Exercise/03_fast_food.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+quantity = int(input())
+orders = deque(list(map(int, input().split())))
+
+print(max(orders))
+
+while orders:
+    some_order = orders.popleft()
+    if some_order <= quantity:
+        quantity -= some_order
+    else:
+        orders.appendleft(some_order)
+        break
+
+if orders:
+    print(f"Orders left: {' '.join(str(n) for n in orders)}")
+else:
+    print(f"Orders complete")
