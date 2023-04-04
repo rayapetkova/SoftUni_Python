af9b6bfa83c9054e241ca0b0430e8dacd6791d44
rayapetkova <rayapetkova2007@abv.bg>
2023-04-04 14:59:15 +0300

    Update 02. task

---
+++ 3. Python Advanced/2. Lists as Stacks and Queues - Exercise/02_stacked_queries.py
@@ -0,0 +1,27 @@
+def check_stack_line_length(some_stack):
+    if some_stack:
+        return True
+    return False
+
+
+n = int(input())
+stack_line = []
+
+for i in range(n):
+    command = input().split()
+    if "1" in command:
+        stack_line.append(int(command[1]))
+    elif check_stack_line_length(stack_line):
+        if "2" in command:
+            stack_line.pop()
+        elif "3" in command:
+            print(max(stack_line))
+        elif "4" in command:
+            print(min(stack_line))
+
+while stack_line:
+    element = stack_line.pop()
+    if check_stack_line_length(stack_line):
+        print(element, end=", ")
+    else:
+        print(element, end="")
