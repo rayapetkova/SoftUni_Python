96d642fb4fb4ee70ef6c9abfd3eb47dc535732bc
rayapetkova <rayapetkova2007@abv.bg>
2023-04-05 00:33:29 +0300

    Update 06. task

---
+++ 3. Python Advanced/2. Lists as Stacks and Queues - Exercise/06_balance_parentheses.py
@@ -0,0 +1,30 @@
+first, second, third = ["{", "}"], ["(", ")"], ["[", "]"]
+
+stack_line = []
+expression = input()
+found = False
+
+for element in expression:
+    if element in "{([":
+        stack_line.append(element)
+    else:
+        if stack_line:
+            popped_element = stack_line.pop()
+            if element == first[1] and popped_element == first[0]:
+                found = False
+            elif element == second[1] and popped_element == second[0]:
+                found = False
+            elif element == third[1] and popped_element == third[0]:
+                found = False
+            else:
+                stack_line.append(popped_element)
+                found = True
+                break
+        else:
+            found = True
+            break
+
+if stack_line or found:
+    print("NO")
+else:
+    print(f"YES")
