51f797ae4639bb3dc5b53f4bca6cb6dd1675dd12
rayapetkova <rayapetkova2007@abv.bg>
2023-04-05 00:39:13 +0300

    Update 05. task

---
+++ 3. Python Advanced/2. Lists as Stacks and Queues - Exercise/05_truck_tour.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+n = int(input())
+stack_line = deque()
+
+for i in range(n):
+    command = input().split()
+    petrol, distance = int(command[0]), int(command[1])
+    lst = [petrol, distance]
+    stack_line.append(lst)
+
+for j in range(n):
+    total = 0
+    found = True
+    for pair in stack_line:
+        total = total + pair[0] - pair[1]
+        if total < 0:
+            stack_line.append(stack_line.popleft())
+            found = False
+            break
+    if found:
+        print(j)
+        break
