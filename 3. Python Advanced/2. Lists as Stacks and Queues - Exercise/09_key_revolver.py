a3c87e5b679f0f4bb9570b3f0c4f105906fd27d5
rayapetkova <rayapetkova2007@abv.bg>
2023-04-07 00:05:18 +0300

    Update 09. task

--- 3. Python Advanced/2. Lists as Stacks and Queues - Exercise/09_key_revolver.py
+++ 3. Python Advanced/2. Lists as Stacks and Queues - Exercise/09_key_revolver.py
@@ -1,15 +1,5 @@
 from collections import deque
 
-
-def return_not_bullets_not_locks(curr_intelligence, all_bullets, one_bullet_price, bullets_stack):
-    earned = curr_intelligence - (all_bullets * one_bullet_price)
-    return f"{len(bullets_stack)} bullets left. Earned ${earned}"
-
-
-def return_not_bullets_locks(curr_locks):
-    return f"Couldn't get through. Locks left: {len(curr_locks)}"
-
-
 price_each_bullet = int(input())
 gun_size = int(input())
 gun_barrel_size = gun_size
@@ -19,28 +9,26 @@ intelligence = int(input())
 total_bullets = 0
 
 while True:
-    if gun_barrel_size == 0:
-        if not bullets and locks:
-            print(return_not_bullets_locks(locks))
-            break
-        elif not bullets and not locks:
-            print(return_not_bullets_not_locks(intelligence, total_bullets, price_each_bullet, bullets))
-            break
-        else:
-            gun_barrel_size = gun_size
-            print(f"Reloading!")
-    if (not locks and bullets) or (not locks and not bullets):
-        print(return_not_bullets_not_locks(intelligence, total_bullets, price_each_bullet, bullets))
-        break
-    elif not bullets and locks:
-        print(f"Couldn't get through. Locks left: {len(locks)}")
-        break
-    elif bullets:
+    if gun_barrel_size == 0 and bullets:
+        gun_barrel_size = gun_size
+        print(f"Reloading!")
+    if bullets:
         bullet = bullets.pop()
-        gun_barrel_size -= 1
-        total_bullets += 1
         if bullet <= locks[0]:
             locks.popleft()
             print(f"Bang!")
         else:
             print(f"Ping!")
+        gun_barrel_size -= 1
+        total_bullets += 1
+    if not locks:
+        if bullets and gun_barrel_size == 0:
+            print("Reloading!")
+        earned = intelligence - (total_bullets * price_each_bullet)
+        print(f"{len(bullets)} bullets left. Earned ${earned}")
+        break
+    if not bullets:
+        break
+
+if locks:
+    print(f"Couldn't get through. Locks left: {len(locks)}")
