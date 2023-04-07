c30e76bb43bc97b8f15d6e396f60af0f80222338
rayapetkova <rayapetkova2007@abv.bg>
2023-04-07 15:20:54 +0300

    Update 06. task

--- 2. Python Fundamentals/5. Lists Advanced - Exercise/06_electron_distribution.py
+++ 2. Python Fundamentals/5. Lists Advanced - Exercise/06_electron_distribution.py
@@ -1,21 +1,44 @@
+electrons = int(input())
+
+n = 1
+final = []
+
+while True:
+    result = 2 * n**2
+    if electrons >= result:
+        final.append(result)
+        electrons -= result
+    else:
+        if electrons > 0:
+            final.append(electrons)
+        break
+    n += 1
+
+print(final)
 
 
-def electron_distribution(electrons):
-    shell = 1
-    lst = []
-    while True:
-        if electrons <= 0:
-            break
-        shell_fill = 2 * shell**2
-        if electrons >= shell_fill:
-            lst.append(shell_fill)
-            electrons -= shell_fill
-        else:
-            lst.append(electrons)
-            electrons = 0
-        shell += 1
-    return lst
 
 
-current_electrons = int(input())
-print(electron_distribution(current_electrons))
+
+
+#2
+#
+# def electron_distribution(electrons):
+#     shell = 1
+#     lst = []
+#     while True:
+#         if electrons <= 0:
+#             break
+#         shell_fill = 2 * shell**2
+#         if electrons >= shell_fill:
+#             lst.append(shell_fill)
+#             electrons -= shell_fill
+#         else:
+#             lst.append(electrons)
+#             electrons = 0
+#         shell += 1
+#     return lst
+#
+#
+# current_electrons = int(input())
+# print(electron_distribution(current_electrons))
