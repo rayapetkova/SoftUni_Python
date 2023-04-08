502186c2b10042758d18843403b89a7fffec79a9
rayapetkova <rayapetkova2007@abv.bg>
2023-04-08 16:51:03 +0300

    Update 01. task

---
+++ 3. Python Advanced/3. Tuples and Sets - Lab/01_count_same_values.py
@@ -0,0 +1,39 @@
+numbers = tuple(map(float, input().split()))
+dictionary_nums = {}
+
+for num in numbers:
+    dictionary_nums[num] = numbers.count(num)
+
+for n, times in dictionary_nums.items():
+    print(f"{n} - {times} times")
+
+
+
+
+
+#2
+#
+# numbers = tuple(map(float, input().split()))
+# dictionary_nums = {}
+#
+# for num in numbers:
+#     dictionary_nums[num] = dictionary_nums.get(num, 0) + 1
+#
+# for n, times in dictionary_nums.items():
+#     print(f"{n} - {times} times")
+
+
+
+
+
+#3
+#
+# numbers = tuple(map(float, input().split()))
+# final = []
+#
+# for num in numbers:
+#     if f"{num:.1f} - {numbers.count(num)} times" not in final:
+#         final.append(f"{num:.1f} - {numbers.count(num)} times")
+#
+# for element in final:
+#     print(element)
