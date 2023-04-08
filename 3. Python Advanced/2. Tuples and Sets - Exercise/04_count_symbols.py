c4b49a71986df369daa8d07eebf0ce94d10db607
rayapetkova <rayapetkova2007@abv.bg>
2023-04-08 21:27:49 +0300

    Update 04. task

---
+++ 3. Python Advanced/2. Tuples and Sets - Exercise/04_count_symbols.py
@@ -0,0 +1,25 @@
+text = input()
+dictionary_characters = {}
+
+for element in text:
+    dictionary_characters[element] = text.count(element)
+
+sorted_dict = sorted(dictionary_characters.items())
+for char, count_times in sorted_dict:
+    print(f"{char}: {count_times} time/s")
+
+
+
+
+
+#2
+#
+# text = input()
+# dictionary_characters = {}
+#
+# for element in text:
+#     dictionary_characters[element] = dictionary_characters.get(element, 0) + 1
+#
+# sorted_dict = sorted(dictionary_characters.items())
+# for char, count_times in sorted_dict:
+#     print(f"{char}: {count_times} time/s")
