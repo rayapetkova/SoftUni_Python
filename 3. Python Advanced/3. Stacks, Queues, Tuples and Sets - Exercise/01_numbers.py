f140c38f1daa9643e1fdf18fcbce495f99f23f03
rayapetkova <rayapetkova2007@abv.bg>
2023-04-09 20:39:52 +0300

    Update 01. task

--- 3. Python Advanced/3. Stacks, Queues, Tuples and Sets - Exercise/01_numbers.py
+++ 3. Python Advanced/3. Stacks, Queues, Tuples and Sets - Exercise/01_numbers.py
@@ -5,21 +5,17 @@ num = int(input())
 for i in range(num):
     line = input().split()
     if line[0] == "Add" and line[1] == "First":
-        for el in line:
-            if el.isdigit():
-                first.add(int(el))
+        for el in line[2:]:
+            first.add(int(el))
     elif line[0] == "Add" and line[1] == "Second":
-        for el in line:
-            if el.isdigit():
-                second.add(int(el))
+        for el in line[2:]:
+            second.add(int(el))
     elif line[0] == "Remove" and line[1] == "First":
-        for el in line:
-            if el.isdigit():
-                first.discard(int(el))
+        for el in line[2:]:
+            first.discard(int(el))
     elif line[0] == "Remove" and line[1] == "Second":
-        for el in line:
-            if el.isdigit():
-                second.discard(int(el))
+        for el in line[2:]:
+            second.discard(int(el))
     elif "Check" in line:
         if first.issubset(second) or second.issubset(first):
             print(True)
@@ -28,3 +24,47 @@ for i in range(num):
 
 print(', '.join(str(n) for n in sorted(first)))
 print(', '.join(str(n) for n in sorted(second)))
+
+
+
+
+
+
+#2 - solved task with functions
+#
+# def adding_elements(command, curr_first, curr_second):
+#     if command[1] == "First":
+#         for el in command[2:]:
+#             curr_first.add(int(el))
+#     elif command[1] == "Second":
+#         for el in command[2:]:
+#             curr_second.add(int(el))
+#
+#
+# def removing_elements(command, curr_first, curr_second):
+#     if command[1] == "First":
+#         for el in command[2:]:
+#             curr_first.discard(int(el))
+#     elif command[1] == "Second":
+#         for el in command[2:]:
+#             curr_second.discard(int(el))
+#
+#
+# first = set(int(n) for n in input().split())
+# second = set(int(n) for n in input().split())
+# num = int(input())
+#
+# for i in range(num):
+#     line = input().split()
+#     if line[0] == "Add":
+#         adding_elements(line, first, second)
+#     elif line[0] == "Remove":
+#         removing_elements(line, first, second)
+#     elif "Check" in line:
+#         if first.issubset(second) or second.issubset(first):
+#             print(True)
+#         else:
+#             print(False)
+#
+# print(', '.join(str(n) for n in sorted(first)))
+# print(', '.join(str(n) for n in sorted(second)))
