022e5b621a3c837662b60af3d07b631aeb02ebf7
rayapetkova <rayapetkova2007@abv.bg>
2023-04-11 12:31:33 +0300

    Update 06. task

---
+++ 3. Python Advanced/4. Multidimensional Lists - Lab/06_symbol_in_matrix.py
@@ -0,0 +1,20 @@
+size = int(input())
+matrix = []
+
+for row in range(size):
+    characters = list(input())
+    matrix.append(characters)
+
+symbol = input()
+found = False
+for row in range(size):
+    for col in range(size):
+        if matrix[row][col] == symbol:
+            found = True
+            print(f"({row}, {col})")
+            break
+    if found:
+        break
+
+if not found:
+    print(f"{symbol} does not occur in the matrix")
