118a94adb67a9c7274d6547cbb4fe2210ea90648
rayapetkova <rayapetkova2007@abv.bg>
2023-04-11 17:32:50 +0300

    Update 01. task

--- 3. Python Advanced/Multidimensional Lists - Exercise 1/01_diagonals.py
+++ 3. Python Advanced/Multidimensional Lists - Exercise 1/01_diagonals.py
@@ -4,12 +4,17 @@ matrix = []
 for row in range(size):
     numbers = [int(n) for n in input().split(", ")]
     matrix.append(numbers)
+
 primary = []
 secondary = []
 
+# this list comprehension can replace 11th and 12th line:
+# [primary.append(matrix[idx][idx]) for idx in range(size)]
 for idx in range(size):
     primary.append(matrix[idx][idx])
 
+# this list comprehension can replace 15th and 16th line:
+# [secondary.append(matrix[idx][size - idx - 1]) for idx in range(size)]
 for idx in range(size):
     secondary.append(matrix[idx][size - idx - 1])
 
