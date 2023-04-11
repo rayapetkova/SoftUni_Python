9e67c8e34a3b4b31d8e00a1d6a766748219d6388
rayapetkova <rayapetkova2007@abv.bg>
2023-04-11 12:13:27 +0300

    Update 04. task

---
+++ 3. Python Advanced/4. Multidimensional Lists - Lab/04_sum_matrix_columns.py
@@ -0,0 +1,12 @@
+rows, columns = [int(n) for n in input().split(", ")]
+matrix = []
+
+for row in range(rows):
+    numbers = [int(num) for num in input().split()]
+    matrix.append(numbers)
+
+for col in range(columns):
+    total = 0
+    for row in range(rows):
+        total += matrix[row][col]
+    print(total)
