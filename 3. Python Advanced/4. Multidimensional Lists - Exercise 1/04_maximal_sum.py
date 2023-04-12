0ac7c7757ac3a7b4843d86d6ceaea6de8fb6bbe3
rayapetkova <rayapetkova2007@abv.bg>
2023-04-12 10:15:20 +0300

    Update 04.task

---
+++ 3. Python Advanced/4. Multidimensional Lists - Exercise 1/04_maximal_sum.py
@@ -0,0 +1,49 @@
+import sys
+
+
+def check_valid_indices(curr_row, curr_col, all_rows, all_cols):
+    if curr_row + 2 >= all_rows or curr_col + 2 >= all_cols:
+        return False
+    return True
+
+
+def get_3x3_list(curr_row, curr_col, curr_matrix):
+    square_list = []
+    for c_row in range(curr_row, curr_row + 3):
+        for c_col in range(curr_col, curr_col + 3):
+            square_list.append(curr_matrix[c_row][c_col])
+    return square_list
+
+
+def print_final_result(curr_dictionary):
+    biggest_key_sum = max(curr_dictionary.keys())
+    print(f"Sum = {biggest_key_sum}")
+    square_matrix_nums = []
+    while dictionary_big_numbers[biggest_key_sum]:
+        square_matrix_nums.append(dictionary_big_numbers[biggest_key_sum][:3])
+        del dictionary_big_numbers[biggest_key_sum][:3]
+    for nested_lst in square_matrix_nums:
+        print(*nested_lst, sep=" ")
+
+
+rows, cols = [int(n) for n in input().split()]
+matrix = []
+
+for row in range(rows):
+    numbers = [int(n) for n in input().split()]
+    matrix.append(numbers)
+
+biggest_number = -sys.maxsize
+dictionary_big_numbers = {}
+
+for row in range(rows):
+    for col in range(cols):
+        if not check_valid_indices(row, col, rows, cols):
+            break
+        square_list_nums = get_3x3_list(row, col, matrix)
+        sum_square_list = sum(square_list_nums)
+        if sum_square_list > biggest_number:
+            biggest_number = sum_square_list
+            dictionary_big_numbers[sum_square_list] = square_list_nums
+
+print_final_result(dictionary_big_numbers)
