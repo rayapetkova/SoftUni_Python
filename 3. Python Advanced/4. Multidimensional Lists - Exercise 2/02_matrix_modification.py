3e9072cb515744677e3fc7d93d7a721d92315897
rayapetkova <rayapetkova2007@abv.bg>
2023-04-15 18:00:19 +0300

    Update 02. task

---
+++ 3. Python Advanced/4. Multidimensional Lists - Exercise 2/02_matrix_modification.py
@@ -0,0 +1,25 @@
+def check_valid_indices(c_row, c_col):
+    if 0 <= c_row < len(matrix) and 0 <= c_col < len(matrix):
+        return True
+    return False
+
+
+rows = int(input())
+matrix = [[int(n) for n in input().split()] for i in range(rows)]
+
+while True:
+    line = input()
+    if line == "END":
+        break
+    command = line.split()
+    row, col, value = int(command[1]),int(command[2]), int(command[3])
+    if not check_valid_indices(row, col):
+        print(f"Invalid coordinates")
+        continue
+    if "Add" in command:
+        matrix[row][col] += value
+    elif "Subtract" in command:
+        matrix[row][col] -= value
+
+for nested in matrix:
+    print(*nested, sep=" ")
