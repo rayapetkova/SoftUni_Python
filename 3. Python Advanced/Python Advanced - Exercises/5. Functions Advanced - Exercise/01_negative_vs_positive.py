c3367508a2413ef415f6578cb9883865c640c7d5
rayapetkova <rayapetkova2007@abv.bg>
2023-04-23 20:42:10 +0300

    Update 01. task

---
+++ 3. Python Advanced/Python Advanced - Exercises/5. Functions Advanced - Exercise/01_negative_vs_positive.py
@@ -0,0 +1,13 @@
+def negatives_and_positives(*args):
+    result_string = ""
+    negatives_sum = sum([num for num in args if num < 0])
+    positives_sum = sum([num for num in args if num > 0])
+    if abs(negatives_sum) > positives_sum:
+        result_string = "The negatives are stronger than the positives"
+    else:
+        result_string = "The positives are stronger than the negatives"
+    return f"{negatives_sum}\n{positives_sum}\n{result_string}"
+
+
+numbers = [int(n) for n in input().split()]
+print(negatives_and_positives(*numbers))
