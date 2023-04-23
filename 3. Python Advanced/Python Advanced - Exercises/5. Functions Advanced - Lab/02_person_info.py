a5dec76fe8515a2d45cf261635f183d1557490c5
rayapetkova <rayapetkova2007@abv.bg>
2023-04-23 17:11:00 +0300

    Update 02. task

---
+++ 3. Python Advanced/Python Advanced - Exercises/5. Functions Advanced - Lab/02_person_info.py
@@ -0,0 +1,18 @@
+def get_info(name, town, age):
+    return f"This is {name} from {town} and he is {age} years old"
+
+
+print(get_info(**{"name": "George", "town": "Sofia", "age": 20}))
+
+
+
+
+
+
+# 2 - this is not a solution for the task, but it's another way to do it (with kwargs)
+#
+# def get_info(**kwargs):
+#     return f"This is {kwargs['name']} from {kwargs['town']} and he is {kwargs['age']} years old"
+#
+#
+# print(get_info(name="George", town="Sofia", age=20))
