d5a68bcbeb3ce27022907c5ef13a8731b828462c
rayapetkova <rayapetkova2007@abv.bg>
2023-04-23 18:35:43 +0300

    Update 05. task

---
+++ 3. Python Advanced/Python Advanced - Exercises/5. Functions Advanced - Lab/05_operate.py
@@ -0,0 +1,39 @@
+from functools import reduce
+import operator
+
+
+def operate(c_operator, *args):
+    operations = {
+        "+": reduce(operator.add, args),
+        "-": reduce(operator.sub, args),
+        "*": reduce(operator.mul, args),
+        "/": reduce(operator.truediv, args)
+    }
+    return operations[c_operator]
+
+# Test inputs:
+# print(operate("+", 1, 2, 3))
+# print(operate("*", 3, 4))
+
+
+
+
+
+#2
+#
+# def operate(operator, *args):
+#     total = 1
+#     if operator == "+":
+#         total = sum(args)
+#     elif operator == "-":
+#         total = args[0]
+#         for num in args[1:]:
+#             total -= num
+#     elif operator == "*":
+#         for num in args:
+#             total *= num
+#     elif operator == "/":
+#         total = args[0]
+#         for num in args[1:]:
+#             total /= num
+#     return total
