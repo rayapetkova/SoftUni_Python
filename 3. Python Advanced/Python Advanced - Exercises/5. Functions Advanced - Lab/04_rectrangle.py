4b066a80508cbf7747d16f381875ca49e6dba7e7
rayapetkova <rayapetkova2007@abv.bg>
2023-04-23 17:49:26 +0300

    Update 04. task

---
+++ 3. Python Advanced/Python Advanced - Exercises/5. Functions Advanced - Lab/04_rectrangle.py
@@ -0,0 +1,15 @@
+def rectangle(a, b):
+    if not (isinstance(a, int) and isinstance(b, int)):
+        return f"Enter valid values!"
+
+    def area(curr_a, curr_b):
+        return curr_a * curr_b
+
+    def perimeter(curr_a, curr_b):
+        return 2*curr_a + 2*curr_b
+
+    return f"Rectangle area: {area(a, b)}\nRectangle perimeter: {perimeter(a, b)}"
+
+# Test inputs:
+# print(rectangle(2, 10))
+# print(rectangle('2', 10))
