32283bfb5b04b7ebe0964afef5f5a7b1934ac6ec
rayapetkova <rayapetkova2007@abv.bg>
2023-04-23 17:40:02 +0300

    Update 01. task

--- 3. Python Advanced/Python Advanced - Exercises/5. Functions Advanced - Lab/01_multiplication_function.py
+++ 3. Python Advanced/Python Advanced - Exercises/5. Functions Advanced - Lab/01_multiplication_function.py
@@ -3,3 +3,9 @@ def multiply(*args):
     for el in args:
         total *= el
     return total
+
+
+# Test inputs:
+# print(multiply(1, 4, 5))
+# print(multiply(4, 5, 6, 1, 3))
+# print(multiply(2, 0, 1000, 5000))
