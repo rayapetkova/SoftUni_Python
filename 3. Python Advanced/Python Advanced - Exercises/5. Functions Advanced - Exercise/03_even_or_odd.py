1faa39f66cdffd5d654c5a8a77745d653c2e8b77
rayapetkova <rayapetkova2007@abv.bg>
2023-04-23 21:06:39 +0300

    Update 03. task

--- 3. Python Advanced/Python Advanced - Exercises/5. Functions Advanced - Exercise/03_even_or_odd.py
+++ 3. Python Advanced/Python Advanced - Exercises/5. Functions Advanced - Exercise/03_even_or_odd.py
@@ -7,3 +7,15 @@ def even_odd(*args):
 # Test inputs:
 # print(even_odd(1, 2, 3, 4, 5, 6, "even"))
 # print(even_odd(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "odd"))
+
+
+
+
+
+#2
+#
+# def even_odd(*args):
+#     *numbers, command_line = args
+#     if command_line == "even":
+#         return [num for num in numbers if num % 2 == 0]
+#     return [num for num in numbers if num % 2 != 0]
