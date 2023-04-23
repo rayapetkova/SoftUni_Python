0a9112491b01aae908d2b8735be8a04b0d331392
rayapetkova <rayapetkova2007@abv.bg>
2023-04-23 17:40:19 +0300

    Update 02. task

--- 3. Python Advanced/Python Advanced - Exercises/5. Functions Advanced - Lab/02_person_info.py
+++ 3. Python Advanced/Python Advanced - Exercises/5. Functions Advanced - Lab/02_person_info.py
@@ -2,7 +2,7 @@ def get_info(name, town, age):
     return f"This is {name} from {town} and he is {age} years old"
 
 
-# Test print:
+# Test input:
 # print(get_info(**{"name": "George", "town": "Sofia", "age": 20}))
 
 
