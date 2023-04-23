63b1e2a195304d93e12973de09d85ff560af9470
rayapetkova <rayapetkova2007@abv.bg>
2023-04-23 23:36:04 +0300

    Update 05. task

---
+++ 3. Python Advanced/Python Advanced - Exercises/5. Functions Advanced - Exercise/05_concatenate.py
@@ -0,0 +1,9 @@
+def concatenate(*args, **kwargs):
+    result = "".join(args)
+    for word in kwargs.keys():
+        result = result.replace(word, kwargs[word])
+    return result
+
+
+# print(concatenate("Soft", "UNI", "Is", "Grate", "!", UNI="Uni", Grate="Great"))
+# print(concatenate("I", " ", "Love", " ", "Cythons", C="P", s="", java='Java'))
