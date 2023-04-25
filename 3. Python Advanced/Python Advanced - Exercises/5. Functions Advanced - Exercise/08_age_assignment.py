d43d132e6300cd8bdfae43a3e0ed737e824a1080
rayapetkova <rayapetkova2007@abv.bg>
2023-04-25 16:58:29 +0300

    Update 08. task

---
+++ 3. Python Advanced/Python Advanced - Exercises/5. Functions Advanced - Exercise/08_age_assignment.py
@@ -0,0 +1,13 @@
+def age_assignment(*args, **kwargs):
+    final = []
+    for first_letter in kwargs.keys():
+        for name in args:
+            if name.startswith(first_letter):
+                final.append(f"{name} is {kwargs[first_letter]} years old.")
+    sorted_final = list(sorted(final))
+    return "\n".join(sorted_final)
+
+
+# Test inputs:
+# print(age_assignment("Peter", "George", G=26, P=19))
+# print(age_assignment("Amy", "Bill", "Willy", W=36, A=22, B=61))
