b8d5e08c5f1d17354e15ee9cce22453d0d1e99d8
rayapetkova <rayapetkova2007@abv.bg>
2023-04-27 16:03:41 +0300

    Update 01. task

---
+++ 3. Python Advanced/Python Advanced - Exams/Python Advanced Retake Exam - 12 April 2023/01_rubber_duck_debuggers.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+times = deque(int(n) for n in input().split())
+tasks = [int(n) for n in input().split()]
+dictionary = {'Darth Vader Ducky': 0, 'Thor Ducky': 0, 'Big Blue Rubber Ducky': 0, 'Small Yellow Rubber Ducky': 0}
+
+while times and tasks:
+    time = times.popleft()
+    task = tasks.pop()
+    result = time * task
+    if 0 < result <= 60:
+        dictionary['Darth Vader Ducky'] += 1
+    elif 61 <= result <= 120:
+        dictionary['Thor Ducky'] += 1
+    elif 121 <= result <= 180:
+        dictionary['Big Blue Rubber Ducky'] += 1
+    elif 181 <= result <= 240:
+        dictionary['Small Yellow Rubber Ducky'] += 1
+    elif result > 240:
+        task -= 2
+        times.append(time)
+        tasks.append(task)
+
+print(f"Congratulations, all tasks have been completed! Rubber ducks rewarded:")
+print(f"Darth Vader Ducky: {dictionary['Darth Vader Ducky']}"
+      f"\nThor Ducky: {dictionary['Thor Ducky']}"
+      f"\nBig Blue Rubber Ducky: {dictionary['Big Blue Rubber Ducky']}"
+      f"\nSmall Yellow Rubber Ducky: {dictionary['Small Yellow Rubber Ducky']}")
