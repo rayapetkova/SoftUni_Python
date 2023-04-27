cc39de8bdc4a874d59eaae034a60d925ca22c79e
rayapetkova <rayapetkova2007@abv.bg>
2023-04-27 16:42:52 +0300

    Update 03. task

---
+++ 3. Python Advanced/Python Advanced - Exams/Python Advanced Retake Exam - 12 April 2023/03_movie_organizer.py
@@ -0,0 +1,26 @@
+def movie_organizer(*args):
+    dictionary = {}
+    dictionary_length = {}
+    output = []
+    for movie, genre in args:
+        dictionary[genre] = dictionary.get(genre, []) + [movie]
+    for c_genre in dictionary.keys():
+        dictionary_length[c_genre] = dictionary_length.get(c_genre, 0) + len(dictionary[c_genre])
+    sorted_dict_genres = sorted(dictionary_length.items(), key=lambda x: (-x[1], x[0]))
+    for curr_genre, number in sorted_dict_genres:
+        output.append(f"{curr_genre} - {number}")
+        sorted_movies = list(sorted(dictionary[curr_genre]))
+        for c_movie in sorted_movies:
+            output.append(f"* {c_movie}")
+    return "\n".join(output)
+
+
+# print(movie_organizer(
+#     ("The Godfather", "Drama"),
+#     ("The Hangover", "Comedy"),
+#     ("The Shawshank Redemption", "Drama"),
+#     ("The Pursuit of Happiness", "Drama"),
+#     ("The Hangover Part II", "Comedy")))
+
+# print(movie_organizer(
+#     ("The Matrix", "Sci-fi")))
