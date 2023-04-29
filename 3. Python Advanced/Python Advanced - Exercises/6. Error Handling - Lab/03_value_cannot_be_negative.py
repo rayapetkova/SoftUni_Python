4b228b901a150074329eb45eae68c1ea1740a685
rayapetkova <rayapetkova2007@abv.bg>
2023-04-29 19:57:46 +0300

    Update 03. task

---
+++ 3. Python Advanced/Python Advanced - Exercises/6. Error Handling - Lab/03_value_cannot_be_negative.py
@@ -0,0 +1,8 @@
+class ValueCannotBeNegative(Exception):
+    """Value cannot be negative"""
+
+
+for i in range(5):
+    number = int(input())
+    if number < 0:
+        raise ValueCannotBeNegative
