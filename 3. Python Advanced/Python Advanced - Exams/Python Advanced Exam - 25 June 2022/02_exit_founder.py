ff09a8eeb815aea3b6cc95697029081406f29bcf
rayapetkova <rayapetkova2007@abv.bg>
2023-05-03 17:20:01 +0300

    Update 02. task

---
+++ 3. Python Advanced/Python Advanced - Exams/Python Advanced Exam - 25 June 2022/02_exit_founder.py
@@ -0,0 +1,48 @@
+names = input().split(", ")
+SIZE, matrix = 6, []
+tom, jerry = (), ()
+
+for r in range(SIZE):
+    line = input().split()
+    matrix.append(line)
+
+idx = 0
+tom_wall, jerry_wall = False, False
+while True:
+    coordinates = list(input())
+    row, col = int(coordinates[1]), int(coordinates[4])
+    if tom_wall and idx == names.index("Tom"):
+        idx += 1
+        if idx == 2:
+            idx = 0
+        tom_wall = False
+        continue
+    if jerry_wall and idx == names.index("Jerry"):
+        idx += 1
+        if idx == 2:
+            idx = 0
+        jerry_wall = False
+        continue
+    if matrix[row][col] == "E":
+        if idx == names.index("Tom"):
+            player = "Tom"
+        else:
+            player = "Jerry"
+        print(f"{player} found the Exit and wins the game!")
+        break
+    elif matrix[row][col] == "T":
+        if idx == names.index("Tom"):
+            player, winner = "Tom", "Jerry"
+        else:
+            player, winner = "Jerry", "Tom"
+        print(f"{player} is out of the game! The winner is {winner}.")
+        break
+    elif matrix[row][col] == "W":
+        if idx == names.index("Tom"):
+            tom_wall, player = True, "Tom"
+        else:
+            jerry_wall, player = True, "Jerry"
+        print(f"{player} hits a wall and needs to rest.")
+    idx += 1
+    if idx == 2:
+        idx = 0
