05926197cef4c1702cd72c92f91a541fc7fc7a7d
rayapetkova <rayapetkova2007@abv.bg>
2023-05-04 20:01:56 +0300

    Update 03. task

---
+++ 3. Python Advanced/Python Advanced - Exams/Python Advanced Retake Exam - 14 April 2022/03_words_sorting.py
@@ -0,0 +1,21 @@
+def words_sorting(*args):
+    dictionary, final_output = {}, []
+    for word in args:
+        dictionary[word] = sum(ord(symbol) for symbol in word)
+
+    sorted_dict = dictionary
+    if sum(dictionary.values()) % 2 != 0:
+        sorted_dict = sorted(dictionary.items(), key=lambda x: -x[1])
+    else:
+        sorted_dict = sorted(dictionary.items(), key=lambda x: x[0])
+
+    for key, value in sorted_dict:
+        final_output.append(f"{key} - {value}")
+    return '\n'.join(final_output)
+
+print(
+    words_sorting(
+        'escape',
+        'charm',
+        'mythology'
+  ))
