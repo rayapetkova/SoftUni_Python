fa432c0c4447c7e6fa4a674a85c9ae4675979ede
rayapetkova <rayapetkova2007@abv.bg>
2023-05-12 01:26:38 +0300

    Update 05. task

--- 3. Python Advanced/Python Advanced - Exercises/1. Lists as Stacks and Queues - Lab/05_hot_potato.py
+++ 3. Python Advanced/Python Advanced - Exercises/1. Lists as Stacks and Queues - Lab/05_hot_potato.py
@@ -5,12 +5,11 @@ n = int(input()) - 1
 
 idx = 0
 while len(names) > 1:
-    name = names.popleft()
     if idx == n:
-        print(f"Removed {name}")
+        print(f"Removed {names.popleft()}")
         idx = 0
         continue
     idx += 1
-    names.append(name)
+    names.rotate(-1)
 
 print(f"Last is {names.popleft()}")
