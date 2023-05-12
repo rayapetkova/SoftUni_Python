60e8bad7691855b2ed2d1f97004f533cf98a65bf
rayapetkova <rayapetkova2007@abv.bg>
2023-05-12 21:51:34 +0300

    Update 06. task

--- 3. Python Advanced/Python Advanced - Exercises/2. Tuples and Sets - Exercise/06_battle_of_names.py
+++ 3. Python Advanced/Python Advanced - Exercises/2. Tuples and Sets - Exercise/06_battle_of_names.py
@@ -6,12 +6,14 @@ for i in range(1, num + 1):
     sum_ascii = 0
     for symbol in name:
         sum_ascii += ord(symbol)
+
     if int(sum_ascii / i) % 2 == 0:
         even_set.add(int(sum_ascii / i))
     else:
         odd_set.add(int(sum_ascii / i))
 
 sum_odd_set, sum_even_set = sum(odd_set), sum(even_set)
+
 if sum_odd_set == sum_even_set:
     print(', '.join(str(el) for el in odd_set.union(even_set)))
 elif sum_odd_set > sum_even_set:
