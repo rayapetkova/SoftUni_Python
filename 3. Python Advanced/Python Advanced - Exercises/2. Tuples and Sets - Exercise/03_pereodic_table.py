43419b62672995b683e99857119a6ca519786be9
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-05-12 13:15:26 +0300

    Update 03. task.py

--- 3. Python Advanced/Python Advanced - Exercises/2. Tuples and Sets - Exercise/03_pereodic_table.py
+++ 3. Python Advanced/Python Advanced - Exercises/2. Tuples and Sets - Exercise/03_pereodic_table.py
@@ -1,9 +1,21 @@
-num = int(input())
-unique_elements = set()
+chemicals = set()
 
-for i in range(num):
+for _ in range(int(input())):
     elements = input().split()
-    for element in elements:
-        unique_elements.add(element)
+    [chemicals.add(el) for el in elements]
 
-[print(el) for el in unique_elements]
+print(*chemicals, sep="\n")
+
+
+
+#2
+#
+# num = int(input())
+# unique_elements = set()
+
+# for i in range(num):
+#     elements = input().split()
+#     for element in elements:
+#         unique_elements.add(element)
+
+# [print(el) for el in unique_elements]
