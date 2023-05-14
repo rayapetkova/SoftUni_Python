0f4f11c021b43d01361828d5e6a8e0286489031d
rayapetkova <rayapetkova2007@abv.bg>
2023-05-14 23:52:08 +0300

    Update 03. task

---
+++ 3. Python Advanced/Python OOP - Exercises/1. First Steps in OOP - Lab/03_class_book.py
@@ -0,0 +1,13 @@
+class Book:
+    def __init__(self, name, author, pages):
+        self.name = name
+        self.author = author
+        self.pages = pages
+
+
+# Test input:
+
+# book = Book("My Book", "Me", 200)
+# print(book.name)
+# print(book.author)
+# print(book.pages)
