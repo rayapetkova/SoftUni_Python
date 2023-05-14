cf7ae1dcf458bef20eb96cca97a583a86c4457a6
rayapetkova <rayapetkova2007@abv.bg>
2023-05-15 00:53:05 +0300

    Update 01. task

---
+++ 3. Python Advanced/Python OOP - Exercises/1. First Steps in OOP - Lab/01_rhombus_of_stars.py
@@ -0,0 +1,14 @@
+def print_half(num, stars):
+    for i in range(num - stars):
+        print(" ", end="")
+    for j in range(1, stars):
+        print(f"* ", end="")
+    print("*")
+
+
+n = int(input())
+
+for first_stars in range(1, n):
+    print_half(n, first_stars)
+for second_stars in range(n, 0, -1):
+    print_half(n, second_stars)
