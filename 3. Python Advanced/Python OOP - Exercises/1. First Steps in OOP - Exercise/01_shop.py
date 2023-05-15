8f6df2581a61ab32c667e69bb2d10ec7d21d11be
rayapetkova <rayapetkova2007@abv.bg>
2023-05-15 22:55:49 +0300

    Update 01. task

---
+++ 3. Python Advanced/Python OOP - Exercises/1. First Steps in OOP - Exercise/01_shop.py
@@ -0,0 +1,13 @@
+class Shop:
+    def __init__(self, name: str, items: list):
+        self.name = name
+        self.items = items
+
+    def get_items_count(self):
+        return len(self.items)
+
+
+# Test code:
+
+# shop = Shop("My Shop", ["Apples", "Bananas", "Cucumbers"])
+# print(shop.get_items_count())
