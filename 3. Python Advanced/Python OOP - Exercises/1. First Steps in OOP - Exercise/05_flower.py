66975ccc03d2b9016a2b74aae9f569eeb4bb1ee2
rayapetkova <rayapetkova2007@abv.bg>
2023-05-17 01:33:46 +0300

    Update 05. task

--- 3. Python Advanced/Python OOP - Exercises/1. First Steps in OOP - Exercise/05_flower.py
+++ 3. Python Advanced/Python OOP - Exercises/1. First Steps in OOP - Exercise/05_flower.py
@@ -11,11 +11,12 @@ class Flower:
     def status(self):
         return f"{self.name} is happy" if self.is_happy else f"{self.name} is not happy"
 
+# Test code:
 
-flower = Flower("Lilly", 100)
-flower.water(50)
-print(flower.status())
-flower.water(60)
-print(flower.status())
-flower.water(100)
-print(flower.status())
+# flower = Flower("Lilly", 100)
+# flower.water(50)
+# print(flower.status())
+# flower.water(60)
+# print(flower.status())
+# flower.water(100)
+# print(flower.status())
