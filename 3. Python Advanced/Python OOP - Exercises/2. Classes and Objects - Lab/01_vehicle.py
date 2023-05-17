6d7a29e8279f7bc8e36f6ff6ba7573a3d364e8f2
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-05-17 23:41:10 +0300

    Update 01. task

---
+++ 3. Python Advanced/Python OOP - Exercises/2. Classes and Objects - Lab/01_vehicle.py
@@ -0,0 +1,15 @@
+class Vehicle:
+    def __init__(self, mileage: int, max_speed=150):
+        self.gadgets = []
+        self.mileage = mileage
+        self.max_speed = max_speed
+
+
+# Test code:
+
+# car = Vehicle(20)
+# print(car.max_speed)
+# print(car.mileage)
+# print(car.gadgets)
+# car.gadgets.append('Hudly Wireless')
+# print(car.gadgets)
