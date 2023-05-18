147f8287d19c83b7f67c6ea5fb97ed8bb4a478b1
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-05-19 00:02:58 +0300

    Update 04. task

--- 3. Python Advanced/Python Advanced - Exercises/4. Multidimensional Lists - Exercise 1/04_maximal_sum.py
+++ 3. Python Advanced/Python Advanced - Exercises/4. Multidimensional Lists - Exercise 1/04_maximal_sum.py
@@ -7,15 +7,18 @@ def check_valid_indices(curr_row, curr_col, all_rows, all_cols):
 
 def get_3x3_list(curr_row, curr_col, curr_matrix):
     square_list = []
+
     for c_row in range(curr_row, curr_row + 3):
         for c_col in range(curr_col, curr_col + 3):
             square_list.append(curr_matrix[c_row][c_col])
+
     return square_list
 
 
 def print_final_result(curr_dictionary):
     biggest_key_sum = max(curr_dictionary.keys())
     print(f"Sum = {biggest_key_sum}")
+
     while dictionary_big_numbers[biggest_key_sum]:
         nested_list = dictionary_big_numbers[biggest_key_sum][:3]
         print(*nested_list, sep=" ")
@@ -36,6 +39,7 @@ for row in range(rows):
     for col in range(cols):
         if not check_valid_indices(row, col, rows, cols):
             break
+
         square_list_nums = get_3x3_list(row, col, matrix)
         sum_square_list = sum(square_list_nums)
         if sum_square_list > biggest_number:
