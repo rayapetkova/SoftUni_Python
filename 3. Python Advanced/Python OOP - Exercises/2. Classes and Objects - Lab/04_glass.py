b3f948e64269037f8250273bfd0193f06c91b16f
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-05-19 23:19:47 +0300

    Update 04. task

---
+++ 3. Python Advanced/Python OOP - Exercises/2. Classes and Objects - Lab/04_glass.py
@@ -0,0 +1,28 @@
+class Glass:
+    capacity = 250
+
+    def __init__(self):
+        self.content = 0
+
+    def fill(self, ml):
+        if self.content + ml < Glass.capacity:
+            self.content += ml
+            return f"Glass filled with {ml} ml"
+        return f"Cannot add {ml} ml"
+
+    def empty(self):
+        self.content = 0
+        return f"Glass is now empty"
+
+    def info(self):
+        return f"{Glass.capacity - self.content} ml left"
+
+
+# Test code:
+
+# glass = Glass()
+# print(glass.fill(100))
+# print(glass.fill(200))
+# print(glass.empty())
+# print(glass.fill(200))
+# print(glass.info())
