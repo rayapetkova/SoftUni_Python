8ab70fbd0b719661d93f6191565a3d57b8113068
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-05-19 23:29:31 +0300

    Update 01. task

--- 3. Python Advanced/Python OOP - Exercises/1. First Steps in OOP - Lab/01_rhombus_of_stars.py
+++ 3. Python Advanced/Python OOP - Exercises/1. First Steps in OOP - Lab/01_rhombus_of_stars.py
@@ -1,8 +1,10 @@
 def print_half(num, stars):
     for i in range(num - stars):
         print(" ", end="")
+
     for j in range(1, stars):
         print(f"* ", end="")
+
     print("*")
 
 
@@ -10,5 +12,6 @@ n = int(input())
 
 for first_stars in range(1, n):
     print_half(n, first_stars)
+
 for second_stars in range(n, 0, -1):
     print_half(n, second_stars)
