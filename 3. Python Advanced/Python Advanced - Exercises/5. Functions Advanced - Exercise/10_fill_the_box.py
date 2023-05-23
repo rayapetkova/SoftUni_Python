ca8b194018cfe8a6f04c5f93b5971423d4708db5
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-05-23 22:26:31 +0300

    Update 10. task

--- 3. Python Advanced/Python Advanced - Exercises/5. Functions Advanced - Exercise/10_fill_the_box.py
+++ 3. Python Advanced/Python Advanced - Exercises/5. Functions Advanced - Exercise/10_fill_the_box.py
@@ -2,26 +2,30 @@ from collections import deque
 
 
 def fill_the_box(height, length, width, *args):
-    box_size = height * length * width
+    capacity = height * length * width
     cubes = deque(args)
-    while True:
+
+    while cubes:
         cube = cubes.popleft()
+
         if cube == "Finish":
-            cubes.clear()
             break
-        if box_size < cube:
-            cube -= box_size
+
+        if capacity < cube:
+            cube -= capacity
             cubes.appendleft(cube)
-            box_size = 0
+            capacity = 0
             break
-        box_size -= cube
-    if box_size:
-        return f"There is free space in the box. You could put {box_size} more cubes."
-    cubes.pop() if cubes else None
-    return f"No more free space! You have {sum(cubes)} more cubes."
+
+        capacity -= cube
+
+    if capacity > 0:
+        return f"There is free space in the box. You could put {capacity} more cubes."
+
+    idx_finish = cubes.index("Finish")
+    return f"No more free space! You have {sum(list(cubes)[:idx_finish])} more cubes."
 
 
-# Test inputs:
 # print(fill_the_box(2, 8, 2, 2, 1, 7, 3, 1, 5, "Finish"))
 # print(fill_the_box(5, 5, 2, 40, 11, 7, 3, 1, 5, "Finish"))
 # print(fill_the_box(10, 10, 10, 40, "Finish", 2, 15, 30))
