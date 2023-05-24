055150df30a26b4beea4d99a78108ee8e702c5b8
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-05-24 12:35:42 +0300

    Update 03. task

---
+++ 3. Python Advanced/Python OOP - Exercises/3. Inheritance - Exercise/03_players_and_monsters/project/hero.py
@@ -0,0 +1,7 @@
+class Hero:
+    def __init__(self, username: str, level: int):
+        self.username = username
+        self.level = level
+
+    def __str__(self):
+        return f"{self.username} of type {Hero.__name__} has level {self.level}"
---
+++ 3. Python Advanced/Python OOP - Exercises/3. Inheritance - Exercise/03_players_and_monsters/project/muse_elf.py
@@ -0,0 +1,6 @@
+from project.elf import Elf
+
+
+class MuseElf(Elf):
+    def __str__(self):
+        return f"{self.username} of type {MuseElf.__name__} has level {self.level}"
