866b1daba28c05c9f38f62badd732c14fa6fb9ec
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-06-03 14:35:19 +0300

    Update 01. task

---
+++ 3. Python Advanced/Python OOP - Exercises/6. Polymorphism and Abstraction - Lab/01_robots.py
@@ -0,0 +1,36 @@
+class Robot:
+    def __init__(self, name):
+        self.name = name
+
+    @staticmethod
+    def sensors_amount():
+        return 1
+
+
+class MedicalRobot(Robot):
+    @staticmethod
+    def sensors_amount():
+        return 6
+
+
+class ChefRobot(Robot):
+    @staticmethod
+    def sensors_amount():
+        return 4
+
+
+class WarRobot(Robot):
+    @staticmethod
+    def sensors_amount():
+        return 12
+
+
+basic_robot = Robot('Robo')
+da_vinci = MedicalRobot('Da Vinci')
+moley = ChefRobot('Moley')
+griffin = WarRobot('Griffin')
+
+print(basic_robot.sensors_amount())
+print(da_vinci.sensors_amount())
+print(moley.sensors_amount())
+print(griffin.sensors_amount())
