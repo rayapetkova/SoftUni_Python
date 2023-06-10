8eaacbbc96b44394a7b5c9b7ed86e58f9bb4facb
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-06-10 15:33:29 +0300

    Update

---
+++ 3. Python Advanced/Python OOP - Exercises/5. Static and Class Methods - Lab/03_integer.py
@@ -0,0 +1,36 @@
+from math import floor
+
+
+class Integer:
+    def __init__(self, value: int):
+        self.value = value
+
+    @classmethod
+    def from_float(cls, float_value):
+        if isinstance(float_value, float):
+            return cls(floor(float_value))
+
+        return f"value is not a float"
+
+    @classmethod
+    def from_roman(cls, value):
+        roman_nums = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        final = 0
+
+
+    @classmethod
+    def from_string(cls, value):
+        if str(value).isdigit():
+            return cls(int(value))
+
+        return f"wrong type"
+
+
+# Test code:
+
+# first_num = Integer(10)
+# print(first_num.value)
+# second_num = Integer.from_roman("IV")
+# print(second_num.value)
+# print(Integer.from_float("2.6"))
+# print(Integer.from_string(2.6))
