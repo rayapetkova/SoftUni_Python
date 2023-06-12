af6e850157e5c2a31b437fe15d16bb2e6df078b5
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-06-12 15:51:56 +0300

    Update 03. task

---
+++ 3. Python Advanced/Python OOP - Exercises/7. SOLID - Lab/03_ducks.py
@@ -0,0 +1,40 @@
+from abc import abstractmethod, ABC
+
+
+class Duck(ABC):
+    @staticmethod
+    @abstractmethod
+    def quack():
+        pass
+
+
+class RubberDuck(Duck):
+    @staticmethod
+    def quack():
+        return "Squeek"
+
+
+class RobotDuck(Duck):
+    HEIGHT = 50
+
+    def __init__(self):
+        self.height = 0
+
+    @staticmethod
+    def quack():
+        return 'Robotic quacking'
+
+    @staticmethod
+    def walk():
+        return 'Robotic walking'
+
+    def fly(self):
+        """can only fly to specific height but
+        when it reaches it starts landing automatically"""
+        if self.height == RobotDuck.HEIGHT:
+            self.land()
+        else:
+            self.height += 1
+
+    def land(self):
+        self.height = 0
