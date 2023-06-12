0e74b56de06cb730660efaabc8af0f9b74fb8738
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-06-12 16:03:54 +0300

    Update 05. task

--- 3. Python Advanced/Python OOP - Exercises/5. Static and Class Methods - Lab/03_integer.py
+++ 3. Python Advanced/Python OOP - Exercises/5. Static and Class Methods - Lab/03_integer.py
@@ -17,6 +17,9 @@ class Integer:
         roman_nums = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         final = 0
 
+        # for i in range(len(value)):
+        #     if i + 1 == len(value)
+
 
     @classmethod
     def from_string(cls, value):
