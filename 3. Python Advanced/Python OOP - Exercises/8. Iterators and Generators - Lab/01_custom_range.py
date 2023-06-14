3c40a66dd2ba03d032746fa856f746f6fd3823f5
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-06-14 19:41:04 +0300

    Update 01. task

---
+++ 3. Python Advanced/Python OOP - Exercises/8. Iterators and Generators - Lab/01_custom_range.py
@@ -0,0 +1,23 @@
+class custom_range:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.start > self.end:
+            raise StopIteration()
+
+        curr_number = self.start
+        self.start += 1
+
+        return curr_number
+
+
+# Test code:
+
+# one_to_ten = custom_range(1, 10)
+# for num in one_to_ten:
+#     print(num)
