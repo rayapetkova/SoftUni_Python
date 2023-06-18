899478e9dcb21263c9d89798a5c5c4ca456a67df
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-06-18 18:35:39 +0300

    Update 03. task

--- 3. Python Advanced/Python Advanced - Exams/Python Advanced Retake Exam - 12 April 2023/03_movie_organizer.py
+++ 3. Python Advanced/Python Advanced - Exams/Python Advanced Retake Exam - 12 April 2023/03_movie_organizer.py
@@ -1,8 +1,10 @@
 def movie_organizer(*args):
     dictionary, dictionary_length, output = {}, {}, []
+
     for movie, genre in args:
         dictionary[genre] = dictionary.get(genre, []) + [movie]
         dictionary_length[genre] = dictionary_length.get(genre, 0) + 1
+
     sorted_dict_genres = sorted(dictionary_length.items(), key=lambda x: (-x[1], x[0]))
 
     for curr_genre, number in sorted_dict_genres:
@@ -10,6 +12,7 @@ def movie_organizer(*args):
         sorted_movies = list(sorted(dictionary[curr_genre]))
         for c_movie in sorted_movies:
             output.append(f"* {c_movie}")
+
     return "\n".join(output)
 
 
