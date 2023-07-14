01815ca47230aebc721b03ce3f0fc598282bd4b5
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-07-14 22:23:32 +0300

    Update

---
+++ 3. Python Advanced/Python OOP - Exercises/8. Iterators and Generators - Exercise/06_fibonacci_generator.py
@@ -0,0 +1,12 @@
+def fibonacci():
+    num1, num2 = 0, 1
+
+    while True:
+        yield num1
+
+        num1, num2 = num2, num1 + num2
+
+
+# generator = fibonacci()
+# for i in range(5):
+#     print(next(generator))
