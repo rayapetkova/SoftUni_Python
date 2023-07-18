4e3f8d869d1b53130e02c7a3f475d9f958e27da9
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-07-18 12:15:49 +0300

    Update 03. task

---
+++ 3. Python Advanced/Python OOP - Exercises/9. Decorators - Lab/03_even_numbers.py
@@ -0,0 +1,14 @@
+def even_numbers(func):
+    def wrapper(numbers):
+        result = func(numbers)
+        return [n for n in result if n % 2 == 0]
+
+    return wrapper
+
+
+@even_numbers
+def get_numbers(numbers):
+    return numbers
+
+
+print(get_numbers([1, 2, 3, 4, 5]))
