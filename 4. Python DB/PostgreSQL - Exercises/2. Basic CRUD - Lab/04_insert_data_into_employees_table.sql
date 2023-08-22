050b2553dfa4d149b9e7c3b7177a3b2fa92c4021
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-08-23 00:28:07 +0300

    Update 04 and 05 tasks

---
+++ 4. Python DB/PostgreSQL - Exercises/2. Basic CRUD - Lab/04_insert_data_into_employees_table.sql
@@ -0,0 +1,7 @@
+INSERT INTO employees(first_name, last_name, job_title, department_id, salary)
+VALUES ('Samantha', 'Young', 'Housekeeping', 4, 900);
+
+INSERT INTO employees(first_name, last_name, job_title, department_id, salary)
+VALUES ('Roger', 'Palmer', 'Waiter', 3, 928.33);
+
+SELECT * FROM employees;
---
+++ 4. Python DB/PostgreSQL - Exercises/2. Basic CRUD - Lab/05_update_salary_and_select.sql
@@ -0,0 +1,6 @@
+UPDATE employees
+SET salary = salary + 100
+WHERE job_title = 'Manager';
+
+SELECT * FROM employees
+WHERE job_title = 'Manager';
