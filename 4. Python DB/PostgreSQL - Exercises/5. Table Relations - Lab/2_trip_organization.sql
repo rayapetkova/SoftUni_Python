2d2847a89b874e35937c3c79df6829a03e4db1ba
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-08-27 11:42:00 +0300

    Update tasks

---
+++ 4. Python DB/PostgreSQL - Exercises/5. Table Relations - Lab/2_trip_organization.sql
@@ -0,0 +1,5 @@
+SELECT driver_id,
+vehicle_type,
+CONCAT(campers.first_name, ' ', campers.last_name)
+FROM vehicles
+INNER JOIN campers ON campers.id = vehicles.driver_id
---
+++ 4. Python DB/PostgreSQL - Exercises/5. Table Relations - Lab/3_softuni_hiking.sql
@@ -0,0 +1,6 @@
+SELECT start_point,
+end_point,
+leader_id,
+CONCAT(campers.first_name, ' ', campers.last_name)
+FROM routes
+JOIN campers ON campers.id = routes.leader_id;
