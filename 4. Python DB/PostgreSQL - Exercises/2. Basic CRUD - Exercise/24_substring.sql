0cfbe577e4ae9a522fa777595b2a3775f03422f4
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-09-17 12:00:39 +0300

    Added new solution

--- 4. Python DB/PostgreSQL - Exercises/2. Basic CRUD - Exercise/24_substring.sql
+++ 4. Python DB/PostgreSQL - Exercises/2. Basic CRUD - Exercise/24_substring.sql
@@ -1,7 +1,20 @@
-CREATE VIEW view_initials AS
-SELECT first_name AS "initial", last_name
+CREATE VIEW view_initials
+AS SELECT
+		  SUBSTRING(first_name FROM 1 FOR 2) AS "initial",
+		  last_name
 FROM employees
 ORDER BY last_name;
 
-UPDATE view_initials
-SET initial = LEFT(initial, 2);
+
+
+
+
+-- second solution:
+
+-- CREATE VIEW view_initials AS
+-- SELECT first_name AS "initial", last_name
+-- FROM employees
+-- ORDER BY last_name;
+--
+-- UPDATE view_initials
+-- SET initial = LEFT(initial, 2);
