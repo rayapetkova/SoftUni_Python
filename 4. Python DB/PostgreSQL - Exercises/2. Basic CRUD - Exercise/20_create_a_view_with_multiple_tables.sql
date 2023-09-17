b263c447ed14471aa37b5ad11658f1927b81e85b
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-09-17 11:25:58 +0300

    added one more solution

--- 4. Python DB/PostgreSQL - Exercises/2. Basic CRUD - Exercise/20_create_a_view_with_multiple_tables.sql
+++ 4. Python DB/PostgreSQL - Exercises/2. Basic CRUD - Exercise/20_create_a_view_with_multiple_tables.sql
@@ -1,7 +1,23 @@
-CREATE VIEW view_addresses AS
-SELECT CONCAT(first_name, ' ', last_name) AS "Full Name",
-	   department_id,
-       CONCAT(addresses.number, ' ', addresses.street) AS "Address"
-FROM employees
-JOIN addresses ON employees.address_id = addresses.id
+CREATE VIEW view_addresses
+AS SELECT
+		  CONCAT(employees.first_name, ' ', employees.last_name) AS "Full Name",
+		  employees.department_id,
+		  CONCAT(addresses.number, ' ', addresses.street) AS "Address"
+FROM employees, addresses
+WHERE employees.address_id = addresses.id
 ORDER BY "Address";
+
+
+
+
+
+
+-- second solution:
+
+-- CREATE VIEW view_addresses AS
+-- SELECT CONCAT(first_name, ' ', last_name) AS "Full Name",
+-- 	   department_id,
+--        CONCAT(addresses.number, ' ', addresses.street) AS "Address"
+-- FROM employees
+-- JOIN addresses ON employees.address_id = addresses.id
+-- ORDER BY "Address";
