c8d491c138701c865773fb68e95e4eb00add01e2
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-09-18 22:43:07 +0300

    added second solutions

--- 4. Python DB/PostgreSQL - Exercises/3. Built-in Functions - Lab/01_find_book_titles.sql
+++ 4. Python DB/PostgreSQL - Exercises/3. Built-in Functions - Lab/01_find_book_titles.sql
@@ -2,3 +2,14 @@ SELECT title
 FROM books
 WHERE title LIKE 'The%'
 ORDER BY id;
+
+
+
+
+
+-- second solution:
+--
+-- SELECT title
+-- FROM books
+-- WHERE LEFT(title, 3) = 'The'
+-- ORDER BY id;
--- 4. Python DB/PostgreSQL - Exercises/3. Built-in Functions - Lab/02_replace_titles.sql
+++ 4. Python DB/PostgreSQL - Exercises/3. Built-in Functions - Lab/02_replace_titles.sql
@@ -6,3 +6,18 @@ SELECT title
 FROM books
 WHERE title LIKE '***%'
 ORDER BY id;
+
+
+
+
+
+-- second solution:
+--
+-- UPDATE books
+-- SET title = REPLACE(title, 'The', '***')
+-- WHERE LEFT(title, 3) = 'The';
+--
+-- SELECT title
+-- FROM books
+-- WHERE LEFT(title, 3) = '***'
+-- ORDER BY id;
