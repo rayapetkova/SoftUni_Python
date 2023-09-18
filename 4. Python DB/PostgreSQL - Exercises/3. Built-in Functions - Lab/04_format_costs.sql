1ed793a32313c98a3f97c534505930dfd2c94726
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-09-18 22:46:28 +0300

    added second solution

--- 4. Python DB/PostgreSQL - Exercises/3. Built-in Functions - Lab/04_format_costs.sql
+++ 4. Python DB/PostgreSQL - Exercises/3. Built-in Functions - Lab/04_format_costs.sql
@@ -1,3 +1,15 @@
-SELECT title, to_char(cost, '99.999') AS "modified_price"
+SELECT title,
+       to_char(cost, '99.999') AS "modified_price"
 FROM books
 ORDER BY id;
+
+
+
+
+
+-- second solution
+--
+-- SELECT title,
+-- 	   ROUND(cost, 3) AS "modified_price"
+-- FROM books
+-- ORDER BY id;
