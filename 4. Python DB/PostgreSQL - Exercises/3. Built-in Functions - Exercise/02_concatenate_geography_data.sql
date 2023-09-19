36dc52e8441fad7f8e936f9edd1ecb77f60f5080
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-09-19 22:02:06 +0300

    Update 02_concatenate_geography_data.sql

--- 4. Python DB/PostgreSQL - Exercises/3. Built-in Functions - Exercise/02_concatenate_geography_data.sql
+++ 4. Python DB/PostgreSQL - Exercises/3. Built-in Functions - Exercise/02_concatenate_geography_data.sql
@@ -1,7 +1,7 @@
 CREATE VIEW view_continents_countries_currencies_details AS
 SELECT CONCAT(continents.continent_name, ': ', continents.continent_code) AS "Continent Details",
-CONCAT(countries.country_name, ' - ', countries.capital, ' - ', countries.area_in_sq_km, ' - ', 'km2') AS "Country Information",
-CONCAT(currencies.description, ' (', currencies.currency_code, ')') AS "Currencies"
+       CONCAT(countries.country_name, ' - ', countries.capital, ' - ', countries.area_in_sq_km, ' - ', 'km2') AS "Country Information",
+       CONCAT(currencies.description, ' (', currencies.currency_code, ')') AS "Currencies"
 FROM continents
 JOIN countries ON continents.continent_code = countries.continent_code
 JOIN currencies ON countries.currency_code = currencies.currency_code
