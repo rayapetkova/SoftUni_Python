0e1fc92fe5f00d41618a72aa9b82819efc090ef0
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-09-23 23:29:36 +0300

    Update

--- 4. Python DB/PostgreSQL - Exercises/4. Data Aggregation - Exercise/11_filter_max_deposit.sql
+++ 4. Python DB/PostgreSQL - Exercises/4. Data Aggregation - Exercise/11_filter_max_deposit.sql
@@ -1,7 +1,8 @@
-SELECT magic_wand_creator,
-MAX(deposit_amount) AS "Max Deposit Amount"
+SELECT
+	   magic_wand_creator,
+	   MAX(deposit_amount) AS "Max Deposit Amount"
 FROM wizard_deposits
-WHERE deposit_amount NOT BETWEEN 20000 AND 40000
 GROUP BY magic_wand_creator
+HAVING MAX(deposit_amount) < 20000 OR MAX(deposit_amount) > 40000
 ORDER BY "Max Deposit Amount" DESC
 LIMIT 3;
