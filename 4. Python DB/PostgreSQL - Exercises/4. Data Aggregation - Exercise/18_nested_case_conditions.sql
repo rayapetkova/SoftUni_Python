d050b42e40c3820d7dcdd99bbbfbb9e99f30ea07
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-09-25 22:48:23 +0300

    Added second solution

--- 4. Python DB/PostgreSQL - Exercises/4. Data Aggregation - Exercise/18_nested_case_conditions.sql
+++ 4. Python DB/PostgreSQL - Exercises/4. Data Aggregation - Exercise/18_nested_case_conditions.sql
@@ -1,16 +1,42 @@
-CREATE VIEW view_performance_rating AS
-SELECT first_name,
-	   last_name,
-	   job_title,
-	   salary,
-	   department_id,
-	   CASE
-	   	   WHEN salary >= 25000 THEN
-		   CASE
-		       WHEN LEFT(job_title, 6) = 'Senior' THEN 'High-performing Senior'
-			   WHEN LEFT(job_title, 6) != 'Senior' THEN 'High-performing Employee'
-		   	   ELSE 'Average-performing'
-		   END
-		   ELSE 'Average-performing'
-	   END AS "performance_rating"
-FROM employees;
+CREATE OR REPLACE VIEW
+			view_performance_rating
+AS SELECT
+		  e.first_name,
+		  e.last_name,
+		  e.job_title,
+		  e.salary,
+		  e.department_id,
+		  CASE
+		  	   WHEN salary >= 25000 THEN
+			   		CASE
+						 WHEN job_title LIKE 'Senior%' THEN 'High-performing Senior'
+						 WHEN job_title NOT LIKE 'Senior%' THEN 'High-performing Employee'
+						 ELSE 'Average-performing'
+					END
+			   ELSE 'Average-performing'
+		  END AS "performance_rating"
+FROM employees AS e;
+
+
+
+
+
+
+
+
+-- second solution
+
+-- CREATE VIEW
+-- 			view_performance_rating
+-- AS SELECT
+-- 		  e.first_name,
+-- 		  e.last_name,
+-- 		  e.job_title,
+-- 		  e.salary,
+-- 		  e.department_id,
+-- 		  CASE
+-- 		  	   WHEN salary >= 25000 AND job_title LIKE 'Senior%' THEN 'High-performing Senior'
+-- 			   WHEN salary >= 25000 AND job_title NOT LIKE 'Senior%' THEN 'High-performing Employee'
+-- 		  	   ELSE 'Average-performing'
+-- 		  END AS "performance_rating"
+-- FROM employees AS e;
