6b4293cff004a4df7ab0f66153920dffa42e3a08
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-09-25 22:46:46 +0300

    Update

--- 4. Python DB/PostgreSQL - Exercises/4. Data Aggregation - Exercise/16_where_project_status.sql
+++ 4. Python DB/PostgreSQL - Exercises/4. Data Aggregation - Exercise/16_where_project_status.sql
@@ -1,8 +1,9 @@
-SELECT project_name,
+SELECT
+	   project_name,
 	   CASE
-	   WHEN start_date IS NULL AND end_date IS NULL THEN 'Ready for development'
-	   WHEN start_date IS NOT NULL AND end_date IS NULL THEN 'In Progress'
-	   ELSE 'Done'
-	   END AS "project_status"
+	   		WHEN start_date IS NULL AND end_date IS NULL THEN 'Ready for development'
+			WHEN start_date IS NOT NULL AND end_date IS NULL THEN 'In Progress'
+			ELSE 'Done'
+	   END
 FROM projects
 WHERE project_name LIKE '%Mountain%';
--- 4. Python DB/PostgreSQL - Exercises/4. Data Aggregation - Exercise/17_having_salary_level.sql
+++ 4. Python DB/PostgreSQL - Exercises/4. Data Aggregation - Exercise/17_having_salary_level.sql
@@ -1,8 +1,9 @@
-SELECT department_id,
-	   COUNT(department_id) AS "num_employees",
+SELECT
+	   department_id,
+	   COUNT(*) AS "num_employees",
 	   CASE
-	   	   WHEN AVG(salary) > 50000 THEN 'Above average'
-		   WHEN AVG(salary) <= 50000 THEN 'Below average'
+	   	    WHEN AVG(salary) > 50000 THEN 'Above average'
+			WHEN AVG(salary) <= 50000 THEN 'Below average'
 	   END AS "salary_level"
 FROM employees
 GROUP BY department_id
