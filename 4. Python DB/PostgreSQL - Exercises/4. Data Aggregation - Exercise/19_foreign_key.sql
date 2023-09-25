da6f2e1dd3a40e3de23c31813cb59bf9f16766bc
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-09-25 22:48:40 +0300

    Added second solution - foreign keys

--- 4. Python DB/PostgreSQL - Exercises/4. Data Aggregation - Exercise/19_foreign_key.sql
+++ 4. Python DB/PostgreSQL - Exercises/4. Data Aggregation - Exercise/19_foreign_key.sql
@@ -2,10 +2,27 @@ CREATE TABLE employees_projects(
 	id SERIAL PRIMARY KEY,
 	employee_id INTEGER,
 	project_id INTEGER,
-	CONSTRAINT fk_employee_id
-		FOREIGN KEY (employee_id)
-		REFERENCES employees(id),
-	CONSTRAINT fk_project_id
-		FOREIGN KEY(project_id)
-		REFERENCES projects(id)
+
+	FOREIGN KEY(employee_id) REFERENCES employees(id),
+	FOREIGN KEY(project_id) REFERENCES projects(id)
 );
+
+
+
+
+
+
+-- second solution
+
+-- CREATE TABLE employees_projects(
+-- 	id SERIAL PRIMARY KEY,
+-- 	employee_id INTEGER,
+-- 	project_id INTEGER,
+--
+-- 	CONSTRAINT fk_employee_id
+-- 		FOREIGN KEY (employee_id)
+-- 		REFERENCES employees(id),
+-- 	CONSTRAINT fk_project_id
+-- 		FOREIGN KEY(project_id)
+-- 		REFERENCES projects(id)
+-- );
