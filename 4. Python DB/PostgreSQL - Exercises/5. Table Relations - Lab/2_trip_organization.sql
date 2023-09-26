2b64f8f27c219cf23a6da0924b91cbe4a607c2be
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-09-26 23:26:39 +0300

    Update 2_trip_organization.sql

--- 4. Python DB/PostgreSQL - Exercises/5. Table Relations - Lab/2_trip_organization.sql
+++ 4. Python DB/PostgreSQL - Exercises/5. Table Relations - Lab/2_trip_organization.sql
@@ -1,5 +1,8 @@
 SELECT driver_id,
-vehicle_type,
-CONCAT(campers.first_name, ' ', campers.last_name)
+       vehicle_type,
+       CONCAT(campers.first_name, 
+              ' ', 
+              campers.last_name)
 FROM vehicles
-INNER JOIN campers ON campers.id = vehicles.driver_id
+INNER JOIN campers 
+ON campers.id = vehicles.driver_id
