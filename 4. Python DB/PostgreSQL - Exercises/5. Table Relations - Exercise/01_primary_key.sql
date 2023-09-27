bc15574cba0ddcd330ba36bf7b03ad35fe2c8cc6
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-09-27 20:10:32 +0300

    Update 01 and 02. task

---
+++ 4. Python DB/PostgreSQL - Exercises/5. Table Relations - Exercise/01_primary_key.sql
@@ -0,0 +1,12 @@
+CREATE TABLE products(
+	product_name VARCHAR(100)
+);
+
+INSERT INTO products(product_name)
+VALUES ('Broccoli'),
+	   ('Shampoo'),
+	   ('Toothpaste'),
+	   ('Candy');
+
+ALTER TABLE products
+ADD COLUMN id SERIAL PRIMARY KEY;
