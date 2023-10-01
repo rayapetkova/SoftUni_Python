9b214a2c2fa465279a64217dad3e56930d544192
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-10-01 22:17:35 +0300

    Update 01 and 03. tasks

--- 4. Python DB/PostgreSQL - Exercises/6. Subqueries and JOINs - Lab/01_towns_addresses.sql
+++ 4. Python DB/PostgreSQL - Exercises/6. Subqueries and JOINs - Lab/01_towns_addresses.sql
@@ -1,7 +1,10 @@
-SELECT addresses.town_id,
-towns.name,
-addresses.address_text
-FROM addresses
-JOIN towns ON towns.town_id = addresses.town_id
-WHERE towns.name in ('San Francisco', 'Sofia', 'Carnation')
-ORDER BY addresses.town_id, addresses.address_id;
+SELECT
+	   a.town_id,
+	   t.name AS "town_name",
+	   a.address_text
+FROM addresses AS "a"
+JOIN towns AS "t"
+	ON a.town_id = t.town_id
+WHERE t.name IN ('San Francisco', 'Sofia', 'Carnation')
+ORDER BY a.town_id ASC,
+		 a.address_id ASC;
--- 4. Python DB/PostgreSQL - Exercises/6. Subqueries and JOINs - Lab/03_employees_projects.sql
+++ 4. Python DB/PostgreSQL - Exercises/6. Subqueries and JOINs - Lab/03_employees_projects.sql
@@ -1,8 +1,10 @@
-SELECT employees_projects.employee_id,
-CONCAT(employees.first_name, ' ', employees.last_name) AS "full_name",
-projects.project_id,
-projects.name
-FROM employees_projects
-JOIN employees ON employees.employee_id = employees_projects.employee_id
-JOIN projects ON projects.project_id = employees_projects.project_id
-WHERE projects.name = 'Classic Vest';
+SELECT ep.employee_id,
+	   CONCAT(e.first_name, ' ', e.last_name) AS "full_name",
+	   ep.project_id,
+	   p.name
+FROM employees_projects AS ep
+JOIN employees AS "e"
+	 ON ep.employee_id = e.employee_id
+JOIN projects AS "p"
+	 ON ep.project_id = p.project_id
+WHERE ep.project_id = 1;
