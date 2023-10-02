d4750d316e988a28c4bfb10323cb31b0ed17b8d9
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-10-02 16:03:56 +0300

    Update 01. task

--- 4. Python DB/PostgreSQL - Exercises/6. Subqueries and JOINs - Exercise/01_booked_for_nights.sql
+++ 4. Python DB/PostgreSQL - Exercises/6. Subqueries and JOINs - Exercise/01_booked_for_nights.sql
@@ -1,5 +1,7 @@
-SELECT CONCAT(apartments.address, ' ', apartments.address_2) AS "Apartment Address",
-bookings.booked_for AS "Nights"
-FROM apartments
-JOIN bookings ON bookings.booking_id = apartments.booking_id
-ORDER BY apartments.apartment_id ASC;
+SELECT
+	   CONCAT(a.address, ' ', a.address_2) AS "apartment_address",
+	   b.booked_for AS "nights"
+FROM apartments AS "a"
+JOIN bookings AS "b"
+	 ON a.booking_id = b.booking_id
+ORDER BY a.apartment_id ASC;
