305d15dbac426eed30e7fa8694bf026bfaa53aa6
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-10-03 22:51:28 +0300

    Update 03. task

--- 4. Python DB/PostgreSQL - Exercises/7. Database Programmability - Lab/03_employees_promotion_by_id.sql
+++ 4. Python DB/PostgreSQL - Exercises/7. Database Programmability - Lab/03_employees_promotion_by_id.sql
@@ -1,13 +1,13 @@
 create or replace procedure sp_increase_salary_by_id(
    id int
-)
-language plpgsql
-as $$
-begin
+) AS
+$$
+BEGIN
 
 	UPDATE employees
     SET salary = salary + (0.05 * salary)
     WHERE employees.employee_id = id;
 
-    commit;
-end;$$
+END;
+$$
+language plpgsql
