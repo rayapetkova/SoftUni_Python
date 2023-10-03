eaf082f3453cc81b662337edcb81bbdb17640e33
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-10-03 22:38:06 +0300

    Update 16. task

---
+++ 4. Python DB/PostgreSQL - Exercises/6. Subqueries and JOINs - Exercise/16_monasteries_by_country.sql
@@ -0,0 +1,51 @@
+CREATE TABLE monasteries(
+	id SERIAL PRIMARY KEY,
+	monastery_name VARCHAR(255),
+	country_code CHAR(2)
+);
+
+
+INSERT INTO monasteries (monastery_name, country_code)
+VALUES ('Rila Monastery "St. Ivan of Rila"', 'BG'),
+	   ('Bachkovo Monastery "Virgin Mary"', 'BG'),
+	   ('Troyan Monastery "Holy Mother''s Assumption"', 'BG'),
+	   ('Kopan Monastery', 'NP'),
+	   ('Thrangu Tashi Yangtse Monastery', 'NP'),
+	   ('Shechen Tennyi Dargyeling Monastery', 'NP'),
+	   ('Benchen Monastery', 'NP'),
+	   ('Southern Shaolin Monastery', 'CN'),
+	   ('Dabei Monastery', 'CN'),
+	   ('Wa Sau Toi', 'CN'),
+	   ('Lhunshigyia Monastery', 'CN'),
+	   ('Rakya Monastery', 'CN'),
+	   ('Monasteries of Meteora', 'GR'),
+	   ('The Holy Monastery of Stavronikita', 'GR'),
+	   ('Taung Kalat Monastery', 'MM'),
+	   ('Pa-Auk Forest Monastery', 'MM'),
+	   ('Taktsang Palphug Monastery', 'BT'),
+	   ('Sümela Monastery', 'TR');
+
+
+ALTER TABLE countries
+ADD COLUMN "three_rivers" BOOLEAN DEFAULT FALSE;
+
+
+UPDATE countries
+SET three_rivers = TRUE
+WHERE (
+		SELECT
+		   	   COUNT(*)
+		FROM countries_rivers AS "cr"
+		GROUP BY cr.country_code
+	    HAVING cr.country_code = countries.country_code
+) >= 3;
+
+
+SELECT
+	   m.monastery_name,
+	   c.country_name
+FROM monasteries AS "m"
+JOIN countries AS "c"
+	 ON c.country_code = m.country_code
+WHERE c.three_rivers IS FALSE
+ORDER BY m.monastery_name ASC;
