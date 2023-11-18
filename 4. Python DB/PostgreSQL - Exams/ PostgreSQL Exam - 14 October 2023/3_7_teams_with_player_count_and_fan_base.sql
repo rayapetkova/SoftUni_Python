0e2948d9b32c8cd78effc9b5cc58a27cf02bfa23
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-11-18 21:19:45 +0200

    Update 7. task

--- 4. Python DB/PostgreSQL - Exams/ PostgreSQL Exam - 14 October 2023/3_7_teams_with_player_count_and_fan_base.sql
+++ 4. Python DB/PostgreSQL - Exams/ PostgreSQL Exam - 14 October 2023/3_7_teams_with_player_count_and_fan_base.sql
@@ -1,10 +1,10 @@
 SELECT
-       t.id,
-       t.NAME,
-       COUNT(p.id) AS "player_count",
-       t.fan_base
+	   t.id AS "team_id",
+	   t.name AS "team_name",
+	   COUNT(p.id) AS "player_count",
+	   t.fan_base
 FROM teams AS "t"
 LEFT JOIN players AS "p" ON p.team_id = t.id
 WHERE t.fan_base > 30000
-GROUP BY t.id, t.NAME, t.fan_base
-ORDER BY player_count DESC, t.fan_base DESC;
+GROUP BY t.id, t.name, t.fan_base
+ORDER BY player_count DESC, fan_base DESC;
