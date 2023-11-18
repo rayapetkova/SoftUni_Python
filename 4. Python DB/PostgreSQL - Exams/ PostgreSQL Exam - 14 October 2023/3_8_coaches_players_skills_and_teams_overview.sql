9495c5a166ba6b1d73aff1ab5b1e7e976712f443
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-11-18 21:28:54 +0200

    Update 8. task

--- 4. Python DB/PostgreSQL - Exams/ PostgreSQL Exam - 14 October 2023/3_8_coaches_players_skills_and_teams_overview.sql
+++ 4. Python DB/PostgreSQL - Exams/ PostgreSQL Exam - 14 October 2023/3_8_coaches_players_skills_and_teams_overview.sql
@@ -1,13 +1,13 @@
 SELECT
-       CONCAT(c.first_name, ' ', c.last_name) AS "coach_full_name",
-       CONCAT(p.first_name, ' ', p.last_name) AS "player_full_name",
-       t.NAME,
-       sd.passing,
-       sd.shooting,
-       sd.speed
+	   CONCAT(c.first_name, ' ', c.last_name) AS "coach_full_name",
+	   CONCAT(p.first_name, ' ', p.last_name) AS "player_full_name",
+	   t.name AS "team_name",
+	   sd.passing,
+	   sd.shooting,
+	   sd.speed
 FROM coaches AS "c"
 JOIN players_coaches AS "pc" ON pc.coach_id = c.id
 JOIN players AS "p" ON p.id = pc.player_id
-JOIN teams AS "t" ON p.team_id = t.id
+JOIN teams AS "t" ON t.id = p.team_id
 JOIN skills_data AS "sd" ON sd.id = p.skills_data_id
 ORDER BY coach_full_name ASC, player_full_name DESC;
