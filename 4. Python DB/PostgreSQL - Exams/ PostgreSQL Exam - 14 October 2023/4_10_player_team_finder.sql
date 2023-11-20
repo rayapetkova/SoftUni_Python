3cec4158367cfc7a60056ef3eba79c0e66bce901
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-11-20 22:16:00 +0200

    Update 04. task

--- 4. Python DB/PostgreSQL - Exams/ PostgreSQL Exam - 14 October 2023/4_10_player_team_finder.sql
+++ 4. Python DB/PostgreSQL - Exams/ PostgreSQL Exam - 14 October 2023/4_10_player_team_finder.sql
@@ -1,30 +1,25 @@
 CREATE OR REPLACE PROCEDURE sp_players_team_name(
-    IN player_name VARCHAR(50),
-    OUT team_name VARCHAR(45)
+	IN player_name VARCHAR(50),
+	OUT team_name VARCHAR(45)
 )
-AS
+as
 $$
    DECLARE
            current_team VARCHAR(45);
-           player_full_name VARCHAR(50);
    BEGIN
          SELECT
-                CONCAT(first_name, ' ', last_name)
-         FROM players
-         INTO player_full_name;
+		        t.name
+		 FROM teams AS "t"
+		 JOIN players AS "p" ON p.team_id = t.id
+		 WHERE CONCAT(p.first_name, ' ', p.last_name) = player_name
+		 INTO current_team;
 
-         SELECT
-                t.NAME
-         FROM players AS "p"
-         JOIN teams AS "t" ON t.id = p.team_id
-         WHERE CONCAT(first_name, ' ', last_name) = player_name
-         INTO current_team;
+		 IF current_team IS NULL THEN
+		    team_name := 'The player currently has no team';
+		 ELSE
+		    team_name := current_team;
+		 END IF;
 
-         IF current_team IS NULL THEN
-            team_name:= 'The player currently has no team';
-         ELSE
-            team_name:= current_team;
-         END IF;
    END;
 $$
 LANGUAGE plpgsql;
