d8e2d0e89563b31c69f019136172aacc789c57bf
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-11-27 22:10:05 +0200

    Update 03. task

--- 3. Python Advanced/Python Advanced - Exams/Python Advanced Exam - 17 June 2023 !/03_enrollment.py
+++ 3. Python Advanced/Python Advanced - Exams/Python Advanced Exam - 17 June 2023 !/03_enrollment.py
@@ -1,29 +1,23 @@
-def gather_credits(number, *args):
-    courses = []
-    total_credits = 0
 
-    for course, credits in args:
-        if number <= 0:
+def gather_credits(needed_credits, *args):
+
+    current_credits, enrolled_courses = 0, []
+    for name, course_credits in args:
+
+        if current_credits >= needed_credits:
             break
 
-        if course in courses:
+        if name in enrolled_courses:
             continue
 
-        number -= credits
-        courses.append(course)
-        total_credits += credits
+        enrolled_courses.append(name)
+        current_credits += course_credits
 
-    final = []
+    if current_credits >= needed_credits:
+        return f"Enrollment finished! Maximum credits: {current_credits}.\nCourses: {', '.join(c for c in sorted(enrolled_courses))}"
 
-    if number <= 0:
-        final.append(f"Enrollment finished! Maximum credits: {total_credits}.")
+    return f"You need to enroll in more courses! You have to gather {needed_credits - current_credits} credits more."
 
-        final.append(f"Courses: {', '.join([c for c in sorted(courses)])}")
-
-    else:
-        return f"You need to enroll in more courses! You have to gather {number} credits more."
-
-    return "\n".join(final)
 
 
 # Test code:
