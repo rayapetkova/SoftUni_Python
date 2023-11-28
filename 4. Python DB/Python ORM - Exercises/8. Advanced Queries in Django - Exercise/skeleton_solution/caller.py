c14819d40333623f23992f05763e4d3622e67330
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-11-28 21:54:37 +0200

    Update caller.py

--- 4. Python DB/Python ORM - Exercises/8. Advanced Queries in Django - Exercise/skeleton_solution/caller.py
+++ 4. Python DB/Python ORM - Exercises/8. Advanced Queries in Django - Exercise/skeleton_solution/caller.py
@@ -8,24 +8,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models
-# from main_app.models import RealEstateListing, VideoGame, Invoice, BillingInfo, Project, Programmer, Technology, Task, \
-#    Exercise
-# from datetime import date
 
 # Create and check models
 
 # Run and print your queries
-
-# Run the custom manager methods
-# action_games = VideoGame.objects.games_by_genre('Action')
-# recent_games = VideoGame.objects.recently_released_games(2019)
-# average_rating = VideoGame.objects.average_rating()
-# highest_rated = VideoGame.objects.highest_rated_game()
-# lowest_rated = VideoGame.objects.lowest_rated_game()
-#
-# # Print the results
-# print(action_games)
-# print(recent_games)
-# print(average_rating)
-# print(highest_rated)
-# print(lowest_rated)
