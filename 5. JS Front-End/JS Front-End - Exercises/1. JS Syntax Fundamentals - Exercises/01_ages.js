7e3088476c5522e40ac18e0c296fe8c44a5b4a37
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-02-21 23:15:26 +0200

    Update 01_ages.js

--- 5. JS Front-End/JS Front-End - Exercises/1. JS Syntax Fundamentals - Exercises/01_ages.js
+++ 5. JS Front-End/JS Front-End - Exercises/1. JS Syntax Fundamentals - Exercises/01_ages.js
@@ -1,23 +1,23 @@
 function typeOfPerson(age) {
-    let type_person = "out of bounds"
+    let typePerson = "out of bounds"
 
     if (age >= 0 && age <= 2) {
-        type_person = "baby"
+        typePerson = "baby"
     }
     else if (age >= 3 && age <= 13) {
-        type_person = "child"
+        typePerson = "child"
     }
     else if (age >= 14 && age <= 19) {
-        type_person = "teenager"
+        typePerson = "teenager"
     }
     else if (age >= 20 && age <= 65) {
-        type_person = "adult"
+        typePerson = "adult"
     }
     else if (age >= 66) {
-        type_person = "elder"
+        typePerson = "elder"
     }
 
-    console.log(type_person)
+    console.log(typePerson)
 }
 
 
