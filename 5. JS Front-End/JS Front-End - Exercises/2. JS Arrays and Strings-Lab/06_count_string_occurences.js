849c028838145ccc769de98133e623746a28ff31
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-02-24 12:11:21 +0200

    Update 06. task

---
+++ 5. JS Front-End/JS Front-End - Exercises/2. JS Arrays and Strings-Lab/06_count_string_occurences.js
@@ -0,0 +1,20 @@
+function findOccurrences(text, word) {
+    let wordsArray = text.split(' ')
+    let occurrences = 0
+
+    for (let wordFromArray of wordsArray) {
+        if (wordFromArray === word ) {
+            occurrences += 1
+        }
+    }
+
+    console.log(occurrences)
+}
+
+
+
+
+
+
+// Test code
+// findOccurrences('This is a word and it also is a sentence', 'is')
