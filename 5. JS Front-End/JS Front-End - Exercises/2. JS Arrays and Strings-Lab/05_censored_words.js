be846b2d7bad2cfb58b95f2fbcde0e1d163e11b5
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-02-24 12:06:26 +0200

    Update 05. task

---
+++ 5. JS Front-End/JS Front-End - Exercises/2. JS Arrays and Strings-Lab/05_censored_words.js
@@ -0,0 +1,14 @@
+function censoreWord(text, word) {
+
+    occurencies = text.split(word).length - 1
+
+    for (let i = 0; i < occurencies; i++) {
+        text = text.replace(word, '*'.repeat(word.length))
+    }
+
+    console.log(text)
+}
+
+
+
+censoreWord('Small oewj smalll small', 'small')
