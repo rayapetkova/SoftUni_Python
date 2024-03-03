0fe618bc1e4070769845b4c9300b502abd089993
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-03-03 14:03:05 +0200

    update

--- 5. JS Front-End/JS Front-End - Exercises/4. Objects and Classes - Exercises/10_class_vehicle.js
+++ 5. JS Front-End/JS Front-End - Exercises/4. Objects and Classes - Exercises/10_class_vehicle.js
@@ -14,12 +14,3 @@ class Vehicle {
         this.fuel -= fuelLoss
     }
 }
-
-
-
-
-let parts = { engine: 6, power: 100 };
-let vehicle = new Vehicle('a', 'b', parts, 200);
-vehicle.drive(100);
-console.log(vehicle.fuel);
-console.log(vehicle.parts.quality);
