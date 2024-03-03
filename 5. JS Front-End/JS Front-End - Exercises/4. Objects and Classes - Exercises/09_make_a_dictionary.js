206aec263bdb8481c15c3fc6158aadfb16a83a58
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-03-03 15:33:24 +0200

    update 09

--- 5. JS Front-End/JS Front-End - Exercises/4. Objects and Classes - Exercises/09_make_a_dictionary.js
+++ 5. JS Front-End/JS Front-End - Exercises/4. Objects and Classes - Exercises/09_make_a_dictionary.js
@@ -10,15 +10,3 @@ function makeDictionary(terms) {
         console.log(`Term: ${key} => Definition: ${value}`)
     }
 }
-
-
-
-
-makeDictionary([
-    '{"Coffee":"A hot drink made from the roasted and ground seeds (coffee beans) of a tropical shrub."}',
-    '{"Bus":"A large motor vehicle carrying passengers by road, typically one serving the public on a fixed route and for a fare."}',
-    '{"Boiler":"A fuel-burning apparatus or container for heating water."}',
-    '{"Tape":"A narrow strip of material, typically used to hold or fasten something."}',
-    '{"Microphone":"An instrument for converting sound waves into electrical energy variations which may then be amplified, transmitted, or recorded."}'
-    ]
-    )
