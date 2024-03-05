02e6465e04810e90b8393b53c37a2f21e368764e
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-03-05 23:09:34 +0200

    Update 06. task

--- 5. JS Front-End/JS Front-End - Exercises/3. Functions and Statements - Lab/06_sign_check.js
+++ 5. JS Front-End/JS Front-End - Exercises/3. Functions and Statements - Lab/06_sign_check.js
@@ -1,8 +1,9 @@
 function typeOfNumberResult(num1, num2, num3) {
-    if (num1 * num2 * num3 < 0) {
+    let multiply = (a, b) => a * b
+
+    if (multiply(multiply(num1, num2), num3) < 0) {
         return 'Negative'
-    }
-    else {
+    } else {
         return 'Positive'
     }
 }
