996f21af0fa150cac4d0be42d7d92b103c6a4ed5
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-03-06 17:25:41 +0200

    Update 07. task

---
+++ 5. JS Front-End/JS Front-End - Exercises/5. DOM Introduction - Lab/07_extract_parenthesis/ExtractParenthesis.js
@@ -0,0 +1,14 @@
+function extract(content) {
+    let text = document.querySelector(`#${content}`).textContent
+    let matches = text.match(/\((.+?)\)/g)
+
+    let extractedMatches = []
+    for (let match of matches) {
+        match = match.replace('(', '')
+        match = match.replace(')', '')
+
+        extractedMatches.push(match)
+    }
+    
+    return extractedMatches.join('; ')
+}
