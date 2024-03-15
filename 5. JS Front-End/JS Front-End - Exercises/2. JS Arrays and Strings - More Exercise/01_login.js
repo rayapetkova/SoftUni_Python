58faa502f22504f346a1841793d767075a9c2328
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-03-15 23:19:49 +0200

    Update 01. task

---
+++ 5. JS Front-End/JS Front-End - Exercises/2. JS Arrays and Strings - More Exercise/01_login.js
@@ -0,0 +1,23 @@
+function checkPassword(infoArr) {
+    let username = infoArr.shift()
+    let reversedUsername = username.split('').reverse().join('')
+    let logged = false
+    
+    for (let index = 0; index < infoArr.length; index++) {
+        if (infoArr[index] === reversedUsername) {
+            logged = true
+            console.log(`User ${username} logged in.`)
+        }
+        
+        if (index === 3 && !logged) {
+            console.log(`User ${username} blocked!`)
+            return
+        }
+
+        if (infoArr[index] !== reversedUsername) {
+            console.log(`Incorrect password. Try again.`)
+        }
+    }
+}
+
+checkPassword(['sunny','rainy','cloudy','sunny','not sunny'])
