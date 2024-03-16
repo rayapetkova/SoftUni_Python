47e3c329f07d8efd6f75348749ab8278f38945d2
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-03-16 13:15:33 +0200

    Update 03. task

---
+++ 5. JS Front-End/JS Front-End - Exercises/3. Functions and Statements - More Exercises/03_points_validation.js
@@ -0,0 +1,23 @@
+function pointsValidation(points) {
+    [x1, y1, x2, y2] = points
+
+    function checkValidDistance(x1, y1, x2, y2) {
+        if (Number.isInteger(Math.sqrt((x2 - x1)**2 + (y2 - y1)**2))) {
+            return 'valid'
+        }
+
+        return 'invalid'  
+    }
+
+    let firstValidation = checkValidDistance(x1, y1, 0, 0)
+    console.log(`{${x1}, ${y1}} to {0, 0} is ${firstValidation}`)
+
+    let secondValidation = checkValidDistance(x2, y2, 0, 0)
+    console.log(`{${x2}, ${y2}} to {0, 0} is ${secondValidation}`)
+
+    let thirdValidation = checkValidDistance(x1, y1, x2, y2)
+    console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is ${thirdValidation}`)
+}
+
+
+pointsValidation([2, 1, 1, 1])
