9e5835050a136a93405be41cce9e9dda5cfe3320
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-03-19 23:12:56 +0200

    Update 01. task

---
+++ 5. JS Front-End/JS Front-End - Exercises/6. DOM Manipulation and Events - Exercise/01. Sections/script.js
@@ -0,0 +1,20 @@
+function create(words) {
+   let allDivs = []
+   let contendDiv = document.getElementById('content')
+
+   for (let word of words) {
+      let div = document.createElement('div')
+      let p = document.createElement('p')
+      p.textContent = word
+      p.style.display = 'none'
+      div.appendChild(p)
+      div.addEventListener('click', clickedDivEvent)
+
+      contendDiv.appendChild(div)
+   }
+
+   function clickedDivEvent(e) {
+      let currentP = e.currentTarget.getElementsByTagName('p')[0]
+      currentP.style.display = 'block'
+   }
+}
