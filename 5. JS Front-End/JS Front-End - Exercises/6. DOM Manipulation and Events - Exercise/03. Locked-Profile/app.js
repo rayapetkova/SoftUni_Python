6b2583d39b91a89817007adbdd47fe70cba01443
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-03-20 14:31:19 +0200

    Update 03. task

---
+++ 5. JS Front-End/JS Front-End - Exercises/6. DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
@@ -0,0 +1,26 @@
+function lockedProfile() {
+    let radioButtons = Array.from(document.querySelectorAll('.profile input[type="radio"]'))
+    
+    for (let button of radioButtons) {
+        let showMoreButton = button.parentNode.querySelector('button')
+        showMoreButton.addEventListener('click', showMoreButtonEvent)
+    }
+
+    function showMoreButtonEvent(e) {
+        let hiddentFields = e.currentTarget.parentNode.querySelector('div')
+        let unlockRadioButton = e.currentTarget.parentNode.querySelectorAll('input[type="radio"]')[1]
+
+        if (e.currentTarget.textContent === 'Show more') {
+            if (unlockRadioButton.checked) {
+                hiddentFields.style.display = 'block'
+                e.currentTarget.textContent = 'Hide it'
+            } 
+        } else {
+            if (unlockRadioButton.checked) {
+                hiddentFields.style.display = 'none'
+                e.currentTarget.textContent = 'Show more'
+            }
+            
+        }
+    }
+}
